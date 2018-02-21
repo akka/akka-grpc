d05beec1637ae3aee184a0e47fd5769cb5224026
Arnout Engelen <arnouten@bzzt.net>
2018-02-21 11:52:12 +0100

    Generate better filename for services

--- server/src/main/scala/akka/http/grpc/ScalaServerCodeGenerator.scala
+++ server/src/main/scala/akka/http/grpc/ScalaServerCodeGenerator.scala
@@ -1,20 +1,19 @@
 package akka.http.grpc
 
 import akka.grpc.gen.CodeGenerator
-import com.google.protobuf.Descriptors.FileDescriptor
+import com.google.protobuf.Descriptors.{ FileDescriptor, ServiceDescriptor }
 import com.google.protobuf.compiler.PluginProtos
 import com.google.protobuf.compiler.PluginProtos.{ CodeGeneratorRequest, CodeGeneratorResponse }
 
 import scala.collection.JavaConverters._
 
 class ScalaServerCodeGenerator extends CodeGenerator {
-
   def name = "grpc-akka-scaladsl"
 
   override def run(request: CodeGeneratorRequest): CodeGeneratorResponse = {
     val b = CodeGeneratorResponse.newBuilder
 
-    // FIXME generate services code here, the data types we want to leave to scalapb
+    // generate services code here, the data types we want to leave to scalapb
 
     val fileDescByName: Map[String, FileDescriptor] =
       request.getProtoFileList.asScala.foldLeft[Map[String, FileDescriptor]](Map.empty) {
@@ -23,16 +22,19 @@ class ScalaServerCodeGenerator extends CodeGenerator {
           acc + (fp.getName -> FileDescriptor.buildFrom(fp, deps))
       }
 
-    request.getFileToGenerateList.asScala.foreach { name ⇒
-      val fileDesc = fileDescByName(name)
-      val responseFile = generateFile(fileDesc)
-      b.addFile(responseFile)
+    for {
+      file ← request.getFileToGenerateList.asScala
+      fileDesc = fileDescByName(file)
+      service ← fileDesc.getServices.asScala
+      file = generateServiceFile(fileDesc, service)
+    } {
+      b.addFile(file)
     }
 
     b.build()
   }
 
-  def generateFile(fileDesc: FileDescriptor): CodeGeneratorResponse.File = {
+  def generateServiceFile(fileDesc: FileDescriptor, serviceDescriptor: ServiceDescriptor): CodeGeneratorResponse.File = {
     val b = CodeGeneratorResponse.File.newBuilder()
 
     //    b.setName(s"${fileDesc.scalaDirectory}/${fileDesc.fileDescriptorObjectName}Foo.scala")
@@ -49,7 +51,7 @@ class ScalaServerCodeGenerator extends CodeGenerator {
     //            .add("}")
     //      }
     //    b.setContent(fp.result)
-    b.setName(s"${fileDesc.getFullName}AkkaGrpcService.scala") // FIXME
+    b.setName(s"${fileDesc.getOptions.getJavaPackage.replace('.', '/')}/${fileDesc.getPackage}/${serviceDescriptor.getName}Service.scala")
     b.setContent(s"// Hello: ${fileDesc.getFullName}")
     b.build
   }
