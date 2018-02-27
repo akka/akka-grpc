4959fbe1bc39fb3ad00bfc1ddfc43f7e97761a3c
Arnout Engelen <arnouten@bzzt.net>
2018-02-27 18:13:50 +0100

    Update sbt-plugin-tester with gradle build
    Make 'sbt compile' work again, 'gradle build' unfortunately fails
    with `> No value has been specified for property 'zincClasspath'.`,
    not sure what is going on there yet.

---
+++ plugin-tester-scala/src/main/scala/io/grpc/examples/helloworld/Greeter.scala
@@ -0,0 +1,14 @@
+package io.grpc.examples.helloworld
+
+import scala.concurrent.Future
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+class Greeter extends GreeterService {
+  override def sayHello(in: HelloRequest): Future[HelloReply] = ???
+
+  override def streamHellos(in: Source[HelloRequest, _]): Source[HelloReply, Any] = ???
+
+  override def itKeepsTalking(in: Source[HelloRequest,_]): Future[HelloReply] = ???
+}
