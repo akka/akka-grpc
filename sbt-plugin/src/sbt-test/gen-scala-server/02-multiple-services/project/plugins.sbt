99e164cc20c892842652fe58fa8f76e1bbbf2377
Arnout Engelen <github@bzzt.net>
2018-03-05 14:58:37 +0100

    Demonstrate how to implement multiple services (#25) (#56)

---
+++ sbt-plugin/src/sbt-test/gen-scala-server/02-multiple-services/project/plugins.sbt
@@ -0,0 +1,3 @@
+addSbtPlugin("com.lightbend.akka.grpc" % "akka-grpc-sbt-plugin" % sys.props("project.version"))
+
+addSbtPlugin("com.lightbend.sbt" % "sbt-javaagent" % "0.1.4")
---
+++ sbt-plugin/src/sbt-test/gen-scala-server/02-multiple-services/src/main/scala/io/akka/grpc/Main.scala
@@ -0,0 +1,41 @@
+package io.akka.grpc
+
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
+import javax.net.ssl.{KeyManagerFactory, SSLContext}
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.{Http2, HttpsConnectionContext}
+import io.akka.grpc.echo.{Echo, EchoServiceHandler}
+import io.akka.grpc.helloworld.{GreeterImpl, GreeterServiceHandler}
+
+object Main extends App {
+  implicit val system = ActorSystem()
+  implicit val mat = ActorMaterializer()
+
+  val echoHandler = EchoServiceHandler(new Echo)
+  val greeterHandler = GreeterServiceHandler(new GreeterImpl)
+
+  Http2().bindAndHandleAsync(
+    echoHandler.orElse(greeterHandler),
+    interface = "localhost",
+    port = 8443,
+    httpsContext = serverHttpContext())
+
+  private def serverHttpContext() = {
+    // never put passwords into code!
+    val password = "abcdef".toCharArray
+
+    val ks = KeyStore.getInstance("PKCS12")
+    ks.load(Option(getClass.getClassLoader.getResourceAsStream("server.p12")).get, password)
+
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(keyManagerFactory.getKeyManagers, null, new SecureRandom)
+
+    new HttpsConnectionContext(context)
+  }
+}
