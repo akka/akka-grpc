e63d81ad8b71222af8a73b50d0448101090f83bd
Arnout Engelen <github@bzzt.net>
2018-05-08 16:42:31 +0200

    Add more pom metadata as required by sonatype (#202)

--- project/Publish.scala
+++ project/Publish.scala
@@ -9,9 +9,6 @@ object NoPublish extends AutoPlugin {
   override def requires = plugins.JvmPlugin
 
   override def projectSettings = Seq(
-    publishArtifact := false,
-    publish := {},
-    publishLocal := {},
     skip in publish := true,
   )
 }
@@ -26,7 +23,13 @@ object Publish extends AutoPlugin {
   override def projectSettings = Seq(
     bintrayOrganization := Some("akka"),
     bintrayPackage := "akka-grpc",
+    homepage := Some(url("https://developer.lightbend.com/docs/akka-grpc/current/")),
+    scmInfo := Some(ScmInfo(url("https://github.com/akka/akka-grpc"), "git@github.com:akka/akka-grpc")),
     licenses := Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+    developers += Developer("contributors",
+      "Contributors",
+      "https://gitter.im/akka/dev",
+      url("https://github.com/akka/akka-grpc/graphs/contributors")),
   )
 }
 
