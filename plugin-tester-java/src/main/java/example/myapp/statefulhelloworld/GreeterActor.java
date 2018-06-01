7916fa7986aefce64bfb20bb60ad6b8f6b75fc3d
Johan Andrén <johan@markatta.com>
2018-06-01 17:28:05 +0300

    Sample of a stateful service added  (#228)

---
+++ plugin-tester-java/src/main/java/example/myapp/statefulhelloworld/GreeterActor.java
@@ -0,0 +1,50 @@
+package example.myapp.statefulhelloworld;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+
+// #actor
+public class GreeterActor extends AbstractActor {
+
+  public static class ChangeGreeting {
+    public final String newGreeting;
+    public ChangeGreeting(String newGreeting) {
+      this.newGreeting = newGreeting;
+    }
+  }
+  public static class GetGreeting {}
+  public static GetGreeting GET_GREETING = new GetGreeting();
+
+  public static class Greeting {
+    public final String greeting;
+    public Greeting(String greeting) {
+      this.greeting = greeting;
+    }
+  }
+
+  public static Props props(final String initialGreeting) {
+    return Props.create(() -> new GreeterActor(initialGreeting));
+  }
+
+  private Greeting greeting;
+
+  public GreeterActor(String initialGreeting) {
+    greeting = new Greeting(initialGreeting);
+  }
+
+  public AbstractActor.Receive createReceive() {
+    return receiveBuilder()
+        .match(GetGreeting.class, this::onGetGreeting)
+        .match(ChangeGreeting.class, this::onChangeGreeting)
+        .build();
+  }
+
+  private void onGetGreeting(GetGreeting get) {
+    getSender().tell(greeting, getSelf());
+  }
+
+  private void onChangeGreeting(ChangeGreeting change) {
+    greeting = new Greeting(change.newGreeting);
+  }
+}
+// #actor
