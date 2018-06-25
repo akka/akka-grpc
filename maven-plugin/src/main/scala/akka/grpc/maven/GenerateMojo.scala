c1f3aa03a136c09fc48a41da57cb08f7afee9a2b
Johan Andrén <johan@markatta.com>
2018-06-25 10:19:35 +0200

    m2e support (#243)

--- maven-plugin/src/main/scala/akka/grpc/maven/GenerateMojo.scala
+++ maven-plugin/src/main/scala/akka/grpc/maven/GenerateMojo.scala
@@ -1,22 +1,62 @@
 package akka.grpc.maven
 
-import java.io.File
+import java.io.{ ByteArrayOutputStream, File, PrintStream }
 
+import akka.grpc.gen.CodeGenerator
 import akka.grpc.gen.javadsl.{ JavaBothCodeGenerator, JavaClientCodeGenerator, JavaServerCodeGenerator }
 import akka.grpc.gen.scaladsl.{ ScalaBothCodeGenerator, ScalaClientCodeGenerator, ScalaServerCodeGenerator }
-import akka.grpc.gen.CodeGenerator
 import javax.inject.Inject
 import org.apache.maven.plugin.AbstractMojo
-import org.slf4j.LoggerFactory
-import protocbridge.{ JvmGenerator, Target }
 import org.apache.maven.project.MavenProject
+import org.sonatype.plexus.build.incremental.BuildContext
+import protocbridge.{ JvmGenerator, Target }
 import scalapb.ScalaPbCodeGenerator
 
 import scala.annotation.tailrec
 import scala.beans.BeanProperty
+import scala.util.control.NoStackTrace
 
-class GenerateMojo @Inject() (project: MavenProject) extends AbstractMojo {
-  val log = LoggerFactory.getLogger(classOf[GenerateMojo])
+object GenerateMojo {
+  val protocVersion = "-v351"
+
+  case class ProtocError(file: String, line: Int, pos: Int, message: String)
+  private val ProtocErrorRegex = """(\w+\.\w+):(\d+):(\d+):\s(.*)""".r
+
+  /** @return a left(parsed error) or a right(original error string) if it cannot be parsed */
+  def parseError(errorLine: String): Either[ProtocError, String] = {
+    errorLine match {
+      case ProtocErrorRegex(file, line, pos, message) =>
+        Left(ProtocError(file, line.toInt, pos.toInt, message))
+      case unknown =>
+        Right(unknown)
+    }
+  }
+
+  private def captureStdOutAnderr[T](block: => T): (String, String, T) = {
+    val errBao = new ByteArrayOutputStream()
+    val errPrinter = new PrintStream(errBao, true, "UTF-8")
+    val outBao = new ByteArrayOutputStream()
+    val outPrinter = new PrintStream(outBao, true, "UTF-8")
+    val originalOut = System.out
+    val originalErr = System.err
+    System.setOut(outPrinter)
+    System.setErr(errPrinter)
+    val t = try {
+      block
+    } finally {
+      System.setOut(originalOut)
+      System.setErr(originalErr)
+    }
+
+    (outBao.toString("UTF-8"), errBao.toString("UTF-8"), t)
+  }
+
+  val akkaGrpcCodeGeneratorSettings = Seq("flat_package")
+
+}
+
+class GenerateMojo @Inject() (project: MavenProject, buildContext: BuildContext) extends AbstractMojo {
+  import GenerateMojo._
 
   @BeanProperty
   var protoPath: String = _
@@ -28,76 +68,106 @@ class GenerateMojo @Inject() (project: MavenProject) extends AbstractMojo {
   var generateServer: Boolean = _
 
   override def execute(): Unit = {
-    val protoDirectory = new File(project.getBasedir, protoPath).getAbsoluteFile
-    if (!protoDirectory.exists()) throw new RuntimeException(s"Configured protoPath [$protoDirectory] does not exist")
-    val schemas = findProtoFiles(protoDirectory)
+    // verify proto dir exists
+    val protoDir = new File(project.getBasedir, protoPath)
+    if (!protoDir.exists()) sys.error("Protobuf sources directory [%s] does not exist".format(protoDir))
 
-    val sourceRoot = "target/generated-sources/akka-grpc-" + language.name().toLowerCase
-    val sourceManaged = new File(sourceRoot)
-    project.addCompileSourceRoot(sourceRoot)
+    // generated sources should be compiled
+    val generatedSourcesDir = "target/generated-sources/akka-grpc-" + language.name().toLowerCase
+    val compileSourceRoot = new File(project.getBasedir, generatedSourcesDir)
+    project.addCompileSourceRoot(generatedSourcesDir)
 
-    val akkaGrpcCodeGeneratorSettings = Seq("flat_package")
-    val targets = language match {
-      case Language.JAVA ⇒
-        val glueGenerator =
-          if (generateServer)
-            if (generateClient) JavaBothCodeGenerator
-            else JavaServerCodeGenerator
-          else JavaClientCodeGenerator
-        Seq[Target](
-          protocbridge.gens.java -> sourceManaged,
-          adaptAkkaGenerator(sourceManaged, glueGenerator, akkaGrpcCodeGeneratorSettings))
-      case Language.SCALA ⇒
-        val glueGenerator =
-          if (generateServer)
-            if (generateClient) ScalaBothCodeGenerator
-            else ScalaServerCodeGenerator
-          else ScalaClientCodeGenerator
-        Seq[Target](
-          (JvmGenerator("scala", ScalaPbCodeGenerator), akkaGrpcCodeGeneratorSettings) → sourceManaged,
-          adaptAkkaGenerator(sourceManaged, glueGenerator, akkaGrpcCodeGeneratorSettings))
-    }
-    val protocVersion = "-v351"
-    val runProtoc: Seq[String] ⇒ Int = args => com.github.os72.protocjar.Protoc.runProtoc(protocVersion +: args.toArray)
-    val includePaths = Seq(protoDirectory)
-
-    val dependentProjectsIncludePaths = Seq.empty
-    val protocOptions = Seq.empty
-    compile(runProtoc, schemas, includePaths ++ dependentProjectsIncludePaths, protocOptions, targets)
+    generate(compileSourceRoot, protoDir)
   }
 
-  private[this] def executeProtoc(protocCommand: Seq[String] => Int, schemas: Set[File], includePaths: Seq[File], protocOptions: Seq[String], targets: Seq[Target]): Int =
+  private def generate(generatedSourcesDir: File, protoDir: File): Unit = {
+
+    val scanner = buildContext.newScanner(protoDir, true)
+    scanner.setIncludes(Array("**/*.proto"))
+    scanner.scan()
+    val schemas = scanner.getIncludedFiles.map(file => new File(protoDir, file))
+      .filter(buildContext.hasDelta)
+      .toSet
+
+    // only build if there are changes to the proto files
+    if (schemas.isEmpty) {
+      getLog.info("No changed or new .proto-files found in [%s], skipping code generation".format(generatedSourcesDir))
+    } else {
+
+      val targets = language match {
+        case Language.JAVA ⇒
+          val glueGenerator =
+            if (generateServer)
+              if (generateClient) JavaBothCodeGenerator
+              else JavaServerCodeGenerator
+            else JavaClientCodeGenerator
+          Seq[Target](
+            protocbridge.gens.java -> generatedSourcesDir,
+            adaptAkkaGenerator(generatedSourcesDir, glueGenerator, akkaGrpcCodeGeneratorSettings))
+        case Language.SCALA ⇒
+          val glueGenerator =
+            if (generateServer)
+              if (generateClient) ScalaBothCodeGenerator
+              else ScalaServerCodeGenerator
+            else ScalaClientCodeGenerator
+          Seq[Target](
+            (JvmGenerator("scala", ScalaPbCodeGenerator), akkaGrpcCodeGeneratorSettings) → generatedSourcesDir,
+            adaptAkkaGenerator(generatedSourcesDir, glueGenerator, akkaGrpcCodeGeneratorSettings))
+      }
+
+      val runProtoc: Seq[String] ⇒ Int = args => com.github.os72.protocjar.Protoc.runProtoc(protocVersion +: args.toArray)
+      val protocOptions = Seq.empty
+
+      compile(runProtoc, schemas, protoDir, protocOptions, targets)
+    }
+  }
+
+  private[this] def executeProtoc(protocCommand: Seq[String] => Int, schemas: Set[File], protoDir: File, protocOptions: Seq[String], targets: Seq[Target]): Int =
     try {
-      val incPath = includePaths.map("-I" + _.getCanonicalPath)
+      val incPath = "-I" + protoDir.getCanonicalPath
       protocbridge.ProtocBridge.run(protocCommand, targets,
-        incPath ++ protocOptions ++ schemas.map(_.getCanonicalPath),
+        Seq(incPath) ++ protocOptions ++ schemas.map(_.getCanonicalPath),
         pluginFrontend = protocbridge.frontend.PluginFrontend.newInstance)
     } catch {
       case e: Exception =>
-        throw new RuntimeException("error occurred while compiling protobuf files: %s" format (e.getMessage), e)
+        throw new RuntimeException("error occurred while compiling protobuf files: %s".format(e.getMessage), e)
     }
 
-  private[this] def compile(protocCommand: Seq[String] => Int, schemas: Set[File], includePaths: Seq[File], protocOptions: Seq[String], targets: Seq[Target]) = {
-    // Sort by the length of path names to ensure that delete parent directories before deleting child directories.
+  private[this] def compile(protocCommand: Seq[String] => Int, schemas: Set[File], protoDir: File, protocOptions: Seq[String], targets: Seq[Target]): Unit = {
+    // Sort by the length of path names to ensure that we have parent directories before sub directories
     val generatedTargetDirs = targets.map(_.outputPath).sortBy(_.getAbsolutePath.length)
     generatedTargetDirs.foreach(_.mkdirs())
-
     if (schemas.nonEmpty && targets.nonEmpty) {
-      log.info("Compiling %d protobuf files to %s".format(schemas.size, generatedTargetDirs.mkString(",")))
-      log.debug("protoc options:")
-      protocOptions.map("\t" + _).foreach(log.debug(_))
-      schemas.foreach(schema => log.info("Compiling schema %s" format schema))
+      getLog.info("Compiling %d protobuf files to %s".format(schemas.size, generatedTargetDirs.mkString(",")))
+      schemas.foreach { schema =>
+        buildContext.removeMessages(schema)
+      }
+      getLog.debug("Compiling schemas [%s]".format(schemas.mkString(",")))
+      getLog.debug("protoc options: %s".format(protocOptions.mkString(",")))
 
-      val exitCode = executeProtoc(protocCommand, schemas, includePaths, protocOptions, targets)
-      if (exitCode != 0)
-        sys.error("protoc returned exit code: %d" format exitCode)
-
-      log.info("Compiling protobuf")
-      generatedTargetDirs.foreach { dir =>
-        log.info("Protoc target directory: %s".format(dir.getAbsolutePath))
+      getLog.info("Compiling protobuf")
+      val (out, err, exitCode) = captureStdOutAnderr {
+        executeProtoc(protocCommand, schemas, protoDir, protocOptions, targets)
+      }
+      if (exitCode != 0) {
+        err.split("\n\r").map(_.trim).map(parseError).foreach {
+          case Left(ProtocError(file, line, pos, message)) =>
+            buildContext.addMessage(new File(protoDir, file), line, pos, message, BuildContext.SEVERITY_ERROR, new RuntimeException("protoc compilation failed") with NoStackTrace)
+          case Right(otherError) =>
+            sys.error(s"protoc exit code $exitCode: $otherError")
+        }
+      } else {
+        if (getLog.isDebugEnabled) {
+          getLog.debug("protoc output: " + out)
+          getLog.debug("protoc stderr: " + err)
+        }
+        generatedTargetDirs.foreach { dir =>
+          getLog.info("Protoc target directory: %s".format(dir.getAbsolutePath))
+          buildContext.refresh(dir)
+        }
       }
     } else if (schemas.nonEmpty && targets.isEmpty) {
-      log.info("Protobufs files found, but PB.targets is empty.")
+      getLog.info("Protobufs files found, but PB.targets is empty.")
     }
   }
 
@@ -105,19 +175,6 @@ class GenerateMojo @Inject() (project: MavenProject) extends AbstractMojo {
     val adapted = new ProtocBridgeCodeGenerator(generator)
     val jvmGenerator = JvmGenerator(generator.name, adapted)
     (jvmGenerator, settings) -> targetPath
-  }
-
-  private def findProtoFiles(dir: File): Set[File] = {
-    @tailrec
-    def find(pending: Array[File], accumulator: Set[File]): Set[File] =
-      if (pending.isEmpty)
-        accumulator
-      else if (pending.head.isDirectory)
-        find(pending.head.listFiles(f ⇒ f.isDirectory || f.getName.endsWith(".proto")) ++ pending.tail, accumulator)
-      else
-        find(pending.tail, accumulator + pending.head)
-
-    find(dir.listFiles(f ⇒ f.isDirectory || f.getName.endsWith(".proto")), Set.empty)
   }
 
 }
