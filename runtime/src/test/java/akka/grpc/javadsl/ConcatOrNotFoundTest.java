af1a830df02502145bb1675cf3c18f11c6762539
Arnout Engelen <arnouten@bzzt.net>
2018-07-04 16:32:45 +0200

    More copyright headers

--- runtime/src/test/java/akka/grpc/javadsl/ConcatOrNotFoundTest.java
+++ runtime/src/test/java/akka/grpc/javadsl/ConcatOrNotFoundTest.java
@@ -1,6 +1,7 @@
 /**
- * Copyright (C) 2018 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package akka.grpc.javadsl;
 
 import akka.http.javadsl.model.HttpRequest;
