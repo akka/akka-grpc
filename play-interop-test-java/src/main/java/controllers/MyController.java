ffa680f38ff2207846caa8e8008634e58c9097b5
Johan Andrén <johan@markatta.com>
2018-07-18 15:44:40 +0200

    Missing headers

--- play-interop-test-java/src/main/java/controllers/MyController.java
+++ play-interop-test-java/src/main/java/controllers/MyController.java
@@ -1,6 +1,7 @@
 /**
- * Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package controllers;
 
 import example.myapp.helloworld.grpc.GreeterServiceClient;
