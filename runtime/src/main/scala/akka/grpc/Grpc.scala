5e743febfa8f3c125dbb6ac2206f01a0ecc78ff0
Dale Wijnand <dale.wijnand@gmail.com>
2018-08-20 16:36:05 +0100

    Remove a "match may not be exhaustive" warning
        [warn] /d/akka-grpc/runtime/src/main/scala/akka/grpc/Grpc.scala:50:89: match may not be exhaustive.
        [warn] It would fail on the following input: Some((x: String forSome x not in ("gzip", "identity")))
        [warn]   private def uncompressor(encoding: Option[String]): Option[ByteString ⇒ ByteString] = encoding match {
        [warn]                                                                                         ^

--- runtime/src/main/scala/akka/grpc/Grpc.scala
+++ runtime/src/main/scala/akka/grpc/Grpc.scala
@@ -51,6 +51,7 @@ object Grpc {
     case None ⇒ None
     case Some("identity") ⇒ None
     case Some("gzip") ⇒ Some(Gzip.uncompress)
+    case Some(enc) ⇒ throw new IllegalArgumentException(s"Unknown encoding $enc")
   }
 
   private class GrpcFramingDecoderStage(uncompressor: Option[ByteString ⇒ ByteString]) extends ByteStringParser[ByteString] {
