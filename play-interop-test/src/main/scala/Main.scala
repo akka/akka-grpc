bb3f75568ebbd8b693389be8a1fdac5d58b1761d
Arnout Engelen <arnouten@bzzt.net>
2018-08-23 15:27:10 +0200

    Remove unused Main.scala file

--- play-interop-test/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
- */
-
-object Main extends App {
-  // TODO initialize guice?
-  //  Injector injector = Guice.createInjector()
-}
