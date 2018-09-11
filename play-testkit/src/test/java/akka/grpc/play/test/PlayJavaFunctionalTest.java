db2378d834ea7307baa476af2dd109e8fdb474b7
Dale Wijnand <dale.wijnand@gmail.com>
2018-09-11 14:24:30 +0100

    First Play Java gRPC functional test

---
+++ play-testkit/src/test/java/akka/grpc/play/test/PlayJavaFunctionalTest.java
@@ -0,0 +1,34 @@
+package akka.grpc.play.test;
+
+import example.myapp.helloworld.grpc.helloworld.*;
+
+import org.junit.*;
+
+import play.*;
+import play.api.routing.*;
+import play.inject.guice.*;
+import play.libs.ws.*;
+import play.test.*;
+
+import static org.junit.Assert.*;
+import static play.inject.Bindings.*;
+
+public final class PlayJavaFunctionalTest extends WithServer {
+
+  @Override
+  protected Application provideApplication() {
+    return new GuiceApplicationBuilder()
+        .overrides(bind(Router.class).to(GreeterServiceImpl.class))
+        .build();
+  }
+
+  private WSRequest wsUrl(final String path) {
+    return WSTestClient.newClient(testServer.port()).url(path);
+  }
+
+  @Test public void returns404OnNonGrpcRequest() throws Exception {
+    final WSResponse rsp = wsUrl("/").get().toCompletableFuture().get();
+    assertEquals(404, rsp.getStatus());
+  }
+
+}
