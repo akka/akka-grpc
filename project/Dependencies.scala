0c33b042b588f638395c00e2617a5b3383dee2c4
Dale Wijnand <dale.wijnand@gmail.com>
2018-09-27 09:48:13 +0100

    Extract scala211/scala212 to Versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,9 @@ import sbt.Keys._
 object Dependencies {
 
   object Versions {
+    val scala211 = "2.11.12"
+    val scala212 = "2.12.6"
+
     val akka = "2.5.16"
     val akkaHttp = "10.1.5"
     val akkaDiscovery = "0.18.0"
