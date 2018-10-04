86c30f685265d4424c6aa7bfefc10fdc1105b15e
Dale Wijnand <dale.wijnand@gmail.com>
2018-10-04 08:39:58 +0100

    Use FakeSSLTools in ssl-config 0.3.4

--- play-testkit/src/main/scala/play/api/test/SelfSigned.scala
+++ play-testkit/src/main/scala/play/api/test/SelfSigned.scala
@@ -7,6 +7,9 @@ package play.api.test
 import java.security.KeyStore
 
 import javax.net.ssl._
+
+import com.typesafe.sslconfig.ssl.FakeSSLTools
+
 import play.core.ApplicationProvider
 import play.core.server.ServerConfig
 import play.core.server.ssl.FakeKeyStore
@@ -23,27 +26,9 @@ object SelfSigned {
    */
   lazy val (sslContext, trustManager): (SSLContext, X509TrustManager) = {
     val keyStore: KeyStore = FakeKeyStore.generateKeyStore
-
-    buildContextAndTrust(keyStore)
+    FakeSSLTools.buildContextAndTrust(keyStore)
   }
 
-  // TODO: this should be moved to `ssl-config`
-  private def buildContextAndTrust(keyStore: KeyStore) = {
-
-    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
-    tmf.init(keyStore)
-    val tms: Array[TrustManager] = tmf.getTrustManagers
-    val x509TrustManager: X509TrustManager = tms(0).asInstanceOf[X509TrustManager]
-
-    val kmf: KeyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
-    kmf.init(keyStore, Array.emptyCharArray)
-    val kms: Array[KeyManager] = kmf.getKeyManagers
-
-    val sslContext: SSLContext = SSLContext.getInstance("TLS")
-    sslContext.init(kms, tms, null)
-
-    (sslContext, x509TrustManager)
-  }
 }
 
 /**
