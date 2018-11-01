0a9040f12882da7a15ea5625b46cef30b0a103fb
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-01 17:06:39 +0100

    Replace RerstartingClient with an always-restart ClientState

---
+++ runtime/src/main/scala/akka/grpc/internal/ClientState.scala
@@ -0,0 +1,99 @@
+package akka.grpc.internal
+
+import java.util.concurrent.atomic.AtomicReference
+
+import akka.Done
+import akka.annotation.InternalApi
+import akka.grpc.GrpcClientSettings
+import akka.stream.{ ActorMaterializer, Materializer }
+import io.grpc.ManagedChannel
+
+import scala.annotation.tailrec
+import scala.concurrent.{ ExecutionContext, Future, Promise }
+import scala.util.Failure
+
+/**
+ * INTERNAL API
+ * Used from generated code so can't be private.
+ *
+ * Client utilities taking care of Channel reconnection and Channel lifecycle in general.
+ */
+@InternalApi
+class ClientState(settings: GrpcClientSettings)(implicit mat: Materializer, ex: ExecutionContext) {
+
+  // private[this] to access it from tests
+  private[this] val internalChannelRef: AtomicReference[InternalChannel] = new AtomicReference[InternalChannel](create())
+  private val closeDemand: Promise[Done] = Promise[Done]()
+
+  mat match {
+    case m: ActorMaterializer =>
+      m.system.whenTerminated.foreach(_ => close())(ex)
+    case _ =>
+  }
+
+  def withChannel[A](f: Future[ManagedChannel] => A): A = {
+    val c = internalChannelRef.get()
+    if (c != null)
+      f(internalChannelRef.get().managedChannel)
+    else // Likely to happen if this is a shared client during shutdown
+      throw new ClientClosedException
+  }
+
+  def closed(): Future[Done] = {
+    // while there's no request to close this RestartingClient, it will continue to restart.
+    // Once there's demand, the `closed` future will redeem flatMapping with the `closed()`
+    // future of the clientRef that's active at that moment.
+    closeDemand.future.flatMap { _ =>
+      val c = internalChannelRef.get()
+      if (c != null)
+        internalChannelRef.get().done
+      else
+        Future.successful(Done)
+    }
+  }
+
+  @tailrec
+  final def close(): Future[Done] = {
+    closeDemand.trySuccess(Done)
+    val c = internalChannelRef.get()
+    if (c != null) {
+      val done = ChannelUtils.close(c)
+      if (internalChannelRef.compareAndSet(c, null.asInstanceOf[InternalChannel]))
+        done
+      else // client has had a ClientConnectionException and been re-created, need to shutdown the new one
+        close()
+    } else
+      Future.successful(Done)
+  }
+
+  def channelFactory: InternalChannel = NettyClientUtils.createChannel(settings)
+
+  private def create(): InternalChannel = {
+    val c: InternalChannel = channelFactory
+    c.done.onComplete {
+      case Failure(_: ClientConnectionException) =>
+        val old = internalChannelRef.get()
+        if (old != null) {
+          val newInternalClient = create()
+          // Only one client is alive at a time. However a close() could have happened between the get() and this set
+          if (!internalChannelRef.compareAndSet(old, newInternalClient)) {
+            // close the newly created client we've been shutdown
+            ChannelUtils.close(newInternalClient)
+          }
+        }
+      case Failure(_) =>
+        close()
+      case _ =>
+      // let all other exceptions and success through
+    }
+    c
+  }
+}
+/**
+ * INTERNAL API
+ * Used from generated code so can't be private.
+ *
+ * Thrown if a withChannel call is called after closing the internal channel
+ */
+@InternalApi
+class ClientClosedException() extends RuntimeException("withChannel called after close()")
---
+++ runtime/src/test/scala/akka/grpc/internal/ClientStateSpec.scala
@@ -0,0 +1,70 @@
+package akka.grpc.internal
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.grpc.GrpcClientSettings
+import akka.stream.ActorMaterializer
+import io.grpc.ConnectivityState._
+import io.grpc.ManagedChannel
+import org.scalatest.concurrent.{ Eventually, ScalaFutures }
+import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, Matchers }
+
+import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future, Promise }
+
+class ClientStateSpec extends AsyncWordSpec with Matchers with ScalaFutures with Eventually with BeforeAndAfterAll {
+
+  implicit val sys = ActorSystem()
+  implicit val mat = ActorMaterializer()
+  implicit val ec = sys.dispatcher
+  implicit val patience = PatienceConfig(timeout = 3.seconds, interval = 15.milliseconds)
+
+  private val mockSettings: GrpcClientSettings = GrpcClientSettings.connectToServiceAt("somehost.com", 3434)
+
+  private def clientState(channelCompletion: Promise[Done] = Promise[Done]()) =
+    new ClientState(mockSettings) {
+      override def channelFactory: InternalChannel = {
+        val channel: Future[ManagedChannel] = Future.successful(new ChannelUtilsSpec.FakeChannel(Stream(IDLE, CONNECTING, READY)))
+        new InternalChannel(channel, channelCompletion)
+      }
+    }
+
+  def userCodeToLiftChannel: Future[ManagedChannel] => ManagedChannel = {
+    eventualChannel =>
+      Await.result(eventualChannel, 5.seconds)
+  }
+
+  "Client State" should {
+    "successfully provide a channel" in {
+      // given a state
+      val state = clientState()
+      // it provides a channel when needed
+      val channel = state withChannel userCodeToLiftChannel
+      channel should not be null
+    }
+    "fail to provide a channel when the client state has been closed" in {
+      // given a state
+      val state = clientState()
+      // it provides a channel when needed
+      val channel = state withChannel userCodeToLiftChannel
+      channel should not be null
+      state.close()
+      assertThrows[ClientClosedException] {
+        state withChannel userCodeToLiftChannel
+      }
+    }
+    "successfully provide a channel after a failure" in {
+      // given a state we can complete from the outside
+      val channelCompletion = Promise[Done]()
+      val state = clientState(channelCompletion)
+      // it provides a channel when needed
+      state withChannel userCodeToLiftChannel
+      // and, if the channel is failed
+      channelCompletion.tryFailure(new ClientConnectionException(s"Unable to establish connection"))
+
+      // the state can still provide new channels.
+      val channel = state withChannel userCodeToLiftChannel
+      channel should not be null
+    }
+  }
+}
