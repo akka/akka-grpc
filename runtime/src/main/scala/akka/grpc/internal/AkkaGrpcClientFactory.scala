686c56f262c4359a00a65bd39844652369fdfa19
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-14 11:53:51 +0100

    Remove unnecessary `create factory method

--- runtime/src/main/scala/akka/grpc/internal/AkkaGrpcClientFactory.scala
+++ runtime/src/main/scala/akka/grpc/internal/AkkaGrpcClientFactory.scala
@@ -19,8 +19,8 @@ object AkkaGrpcClientFactory {
     //    }
     val classT: Class[_] = classTag[T].runtimeClass
     val module: AnyRef = getClass.getClassLoader.loadClass(classT.getName + "$").getField("MODULE$").get(null)
-    val instance = module.asInstanceOf[{ def create(settings: GrpcClientSettings, mat: Materializer, ex: ExecutionContext): T }]
-    instance.create(settings, mat, ex)
+    val instance = module.asInstanceOf[{ def apply(settings: GrpcClientSettings)(implicit mat: Materializer, ex: ExecutionContext): T }]
+    instance(settings)(mat, ex)
   }
 
   /**
