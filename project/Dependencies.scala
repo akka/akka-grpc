43140986d73822666ff7d88903fb3a9ecb602267
Arnout Engelen <arnout@bzzt.net>
2019-01-29 12:29:07 +0100

    Scala 2.13.0-M5 support

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,15 +7,16 @@ object Dependencies {
 
   object Versions {
     val scala212 = "2.12.8"
+    val scala213 = "2.13.0-M5"
 
     val akka = "2.5.20"
     val akkaHttp = "10.1.8"
 
     val grpc = "1.20.0" // checked synced by GrpcVersionSyncCheckPlugin
     val config = "1.3.3"
-    val sslConfig = "0.3.6"
+    val sslConfig = "0.3.7"
 
-    val scalaTest = "3.0.5"
+    val scalaTest = "3.0.6-SNAP6"
 
     val maven = "3.5.4"
   }
