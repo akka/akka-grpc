9dcb8a25e33322755be0881f56fc6844de1fa33c
Arnout Engelen <arnout@bzzt.net>
2019-08-14 08:42:13 +0200

    More updates

--- gradle-plugin/src/main/groovy/akka/grpc/gradle/AkkaGrpcPlugin.groovy
+++ gradle-plugin/src/main/groovy/akka/grpc/gradle/AkkaGrpcPlugin.groovy
@@ -16,7 +16,7 @@ class AkkaGrpcPlugin implements Plugin<Project>, DependencyResolutionListener {
     final String pluginVersion = AkkaGrpcPlugin.class.package.implementationVersion
 
     final String protocVersion = "3.4.0"
-    final String grpcVersion = "1.22.1" // checked synced by GrpcVersionSyncCheckPlugin
+    final String grpcVersion = "1.22.2" // checked synced by GrpcVersionSyncCheckPlugin
 
     Project project
 
