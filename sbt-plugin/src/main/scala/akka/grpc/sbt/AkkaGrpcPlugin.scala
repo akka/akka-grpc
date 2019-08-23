f5278a01b12eb90534cbcdb8b2b1e994271a90b1
James Roper <james@jazzy.id.au>
2019-08-23 15:16:23 +1000

    Use a dedicated managed sources directory
    Fixes #672
    
    Since sbt-protoc deletes all files in its target directories, it's
    inappropriate to use the shared sourceManaged directory, because it will
    potentially clobber the files of other source generators immediately
    after they've generated them.
    
    This gives akka-grpc its own target directory, which is managed
    heirarchically using `target in akkaGrpcCodeGeneratorSettings`.
    Convention would say that it should be `target in PB.recompile`, since
    usually you scope the target setting to the task that generates to it,
    but since Akka doesn't provide `PB.recompile` itself, and sbt-protoc may
    some day decide to define `target in PB.recompile` for its own purposes,
    it's safer to use something that akka-grpc provides itself.
    
    So, the target directory is set to `target/scala_2.12/akka-grpc` at the
    no configuration scope, and then redefined in each configuration (test,
    compile) to be that plus `/main` or `/test`.

--- sbt-plugin/src/main/scala/akka/grpc/sbt/AkkaGrpcPlugin.scala
+++ sbt-plugin/src/main/scala/akka/grpc/sbt/AkkaGrpcPlugin.scala
@@ -93,13 +93,17 @@ object AkkaGrpcPlugin extends AutoPlugin {
         // hack to get our (dirty) hands on a proper sbt logger before running the generators
         generatorLogger.logger = streams.value.log
         (PB.recompile in Test).value
-      })
+      },
+      target in akkaGrpcCodeGeneratorSettings := crossTarget.value / "akka-grpc")
 
   def configSettings(config: Configuration): Seq[Setting[_]] =
     inConfig(config)(
       (if (config == Compile || config == Test) Seq() // already supported by sbt-protoc by default
        else sbtprotoc.ProtocPlugin.protobufConfigSettings) ++
       Seq(
+        target in akkaGrpcCodeGeneratorSettings := (target in akkaGrpcCodeGeneratorSettings).value / Defaults
+            .nameForSrc(configuration.value.name),
+        managedSourceDirectories += (target in akkaGrpcCodeGeneratorSettings).value,
         unmanagedResourceDirectories ++= (resourceDirectories in PB.recompile).value,
         watchSources in Defaults.ConfigGlobal ++= (sources in PB.recompile).value,
         akkaGrpcGenerators := {
@@ -112,9 +116,11 @@ object AkkaGrpcPlugin extends AutoPlugin {
         },
         // configure the proto gen automatically by adding our codegen:
         PB.targets ++=
-          targetsFor(sourceManaged.value, akkaGrpcCodeGeneratorSettings.value, akkaGrpcGenerators.value),
+          targetsFor(
+            (target in akkaGrpcCodeGeneratorSettings).value,
+            akkaGrpcCodeGeneratorSettings.value,
+            akkaGrpcGenerators.value),
         PB.protoSources += sourceDirectory.value / "proto") ++
-
       inTask(PB.recompile)(Seq(
         includeFilter := GlobFilter("*.proto"),
         managedSourceDirectories := Nil,
