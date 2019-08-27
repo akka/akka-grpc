ba358d1fa532e248bc8dcf5cb3c8ba21a65a90e7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-08-27 17:02:11 +0200

    Update akka-discovery, akka-slf4j, ... to 2.5.25 (#675)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val scala212 = "2.12.8"
     val scala213 = "2.13.0"
 
-    val akka = "2.5.24"
+    val akka = "2.5.25"
     val akkaHttp = "10.1.9"
 
     val grpc = "1.23.0" // checked synced by GrpcVersionSyncCheckPlugin
