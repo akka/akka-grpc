63147f274024a3ed1853255ec81c107cb70bb187
Arnout Engelen <arnout@bzzt.net>
2019-11-01 17:00:43 +0100

    Whitelist allowed protoc settings (#720)
    * Whitelist allowed protoc settings
    
    This is needed because the built-in java generator and the scalapb scala
    generator throw an error when an unrecognized option is passed to them.
    
    Added for sbt and maven, not needed yet for the groovy plugin because
    there we don't allow arbitrary settings.

---
+++ codegen/src/main/scala/akka/grpc/gen/ProtocSettings.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.grpc.gen
+
+object ProtocSettings {
+
+  /** Whitelisted options for the built-in Java protoc plugin */
+  val protocJava = Seq("single_line_to_proto_string", "ascii_format_to_string", "retain_source_code_info")
+
+  /** Whitelisted options for the ScalaPB protoc plugin */
+  val scalapb = Seq(
+    "java_conversions",
+    "flat_package",
+    "single_line_to_proto_string",
+    "ascii_format_to_string",
+    "no_lenses",
+    "retain_source_code_info")
+}
