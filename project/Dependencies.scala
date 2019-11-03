a90047f11330f30a53679c9cd2d397334cd5a896
sullis <github@seansullivan.com>
2019-11-03 04:42:27 -0800

    Scala 2.12.10, Scala 2.13.1 (#708)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ import sbt.Keys._
 object Dependencies {
 
   object Versions {
-    val scala212 = "2.12.8"
-    val scala213 = "2.13.0"
+    val scala212 = "2.12.10"
+    val scala213 = "2.13.1"
 
     val akka = "2.5.25"
     val akkaHttp = "10.1.10"
