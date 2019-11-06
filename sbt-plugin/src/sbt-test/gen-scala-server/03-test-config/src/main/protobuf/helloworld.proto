akka/akka-grpc

commit fbf287b8ce7349bfc7c797abc2197b6c3b123aa7

play nice on projects with other sbt-protoc-based plugins (#696)
When AkkaGrpcPlugin is added to a project with other plugins
appending generators to PB.targets, these extra generators could
be ignored, when AkkaGrpcPlugin was injected last (something which
cannot be controlled, unless there is an explicit dependency from
that plugin to AkkaGrpcPlugin via `AutoPlugin#requires`).

I could not find in the original commit 7f2f96c a reason for
overriding PB.targets, so akka-grpc generators are now appended.
Writing a scripted test to guard this behavior is hard as the order
in which plugins are flattened is not guaranteed/deterministic.

sbt-plugin/src/main/scala/akka/grpc/sbt/AkkaGrpcPlugin.scala
@@ -111,7 +111,7 @@ object AkkaGrpcPlugin extends AutoPlugin {
         },
         // configure the proto gen automatically by adding our codegen:
         // FIXME: actually specifying separate Compile and Test target stub and languages does not work #194
-        PB.targets :=
+        PB.targets ++=
           targetsFor(sourceManaged.value, akkaGrpcCodeGeneratorSettings.value, akkaGrpcGenerators.value),
         PB.protoSources += sourceDirectory.value / "proto",
         // include proto files extracted from the dependencies with "protobuf" configuration by default

commit b3d53d02af5001e08fc2ed4501be9c17ea513c30

sbt: improve support for configurations (#702)
* bump sbt-protoc to fix `sbt test:protocGenerate`

Fix regression introduced by fbf287b and reproduced by scripted test:
as targets are now appended rather than set, and previous versions of
sbt-protoc captured Compile targets as part of Test ones through
configuration inheritance, Test targets (based on ScalaPB & akka-grpc
generators) were executed together with the Compile ones during the Test
protocGenerate. This caused protoc to hang because protoc-bridge does
not support using the same generator in multiple targets.

sbt-protoc 0.99.26 fixes this behavior by initializing the Test targets
so they do not capture the Compile ones when the first user ones get
appended. See https://github.com/thesamet/sbt-protoc/issues/108.

* generate code for test proto source files by default

* generators can be (re-)run with per-configuration settings #194

project/Dependencies.scala
@@ -67,8 +67,7 @@ object Dependencies {
   }
 
   object Plugins {
-    // This should follow the version of sbt-protoc ScalaPB is depending on
-    val sbtProtoc = "com.thesamet" % "sbt-protoc" % "0.99.19"
+    val sbtProtoc = "com.thesamet" % "sbt-protoc" % "0.99.26"
   }
 
   private val l = libraryDependencies

sbt-plugin/src/main/scala/akka/grpc/sbt/AkkaGrpcPlugin.scala
@@ -100,6 +100,7 @@ object AkkaGrpcPlugin extends AutoPlugin {
 
   def configSettings(config: Configuration): Seq[Setting[_]] =
     inConfig(config)(
+      sbtprotoc.ProtocPlugin.protobufConfigSettings ++
       Seq(
         unmanagedResourceDirectories ++= (resourceDirectories in PB.recompile).value,
         watchSources in Defaults.ConfigGlobal ++= (sources in PB.recompile).value,
@@ -113,10 +114,11 @@ object AkkaGrpcPlugin extends AutoPlugin {
             toGenerator(g, ScalaBinaryVersion(scalaBinaryVersion.value), generatorLogger))
         },
         // configure the proto gen automatically by adding our codegen:
-        // FIXME: actually specifying separate Compile and Test target stub and languages does not work #194
         PB.targets ++=
           targetsFor(sourceManaged.value, akkaGrpcCodeGeneratorSettings.value, akkaGrpcGenerators.value),
         PB.protoSources += sourceDirectory.value / "proto",
+        // include proto files from parent configurations to be able to run extra generators or generators with different settings
+        PB.protoSources ++= PB.protoSources.all(ancestorConfigsFilter(config)).value.flatten,
         // include proto files extracted from the dependencies with "protobuf" configuration by default
         PB.protoSources += PB.externalIncludePath.value) ++
 
@@ -234,5 +236,12 @@ object AkkaGrpcPlugin extends AutoPlugin {
     }
 
     (outBao.toString("UTF-8"), errBao.toString("UTF-8"), t)
+  }
+
+  private def ancestorConfigsFilter(config: Configuration) = {
+    def ancestors(confs: Vector[Configuration]): Vector[Configuration] =
+      confs ++ confs.flatMap(conf => ancestors(conf.extendsConfigs))
+
+    ScopeFilter(configurations = inConfigurations(ancestors(config.extendsConfigs): _*))
   }
 }

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/build.sbt
@@ -1,0 +1,10 @@
+enablePlugins(AkkaGrpcPlugin)
+
+Compile / akkaGrpcGeneratedSources := Seq(AkkaGrpc.Server)
+
+Test / akkaGrpcGeneratedSources := Seq(AkkaGrpc.Client)
+
+configs(IntegrationTest)
+Defaults.itSettings
+AkkaGrpcPlugin.configSettings(IntegrationTest)
+IntegrationTest / akkaGrpcGeneratedLanguages := Seq(AkkaGrpc.Java)

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/project/plugins.sbt
@@ -1,0 +1,1 @@
+addSbtPlugin("com.lightbend.akka.grpc" % "sbt-akka-grpc" % sys.props("project.version"))

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/src/main/protobuf/echo_message.proto
@@ -1,0 +1,10 @@
+syntax = "proto3";
+
+option java_multiple_files = true;
+option java_package = "example.myapp.echo.grpc";
+
+package echo;
+
+message EchoMessage {
+    string payload = 1;
+}

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/src/main/protobuf/helloworld.proto
@@ -1,0 +1,29 @@
+syntax = "proto3";
+
+option java_multiple_files = true;
+option java_package = "example.myapp.helloworld.grpc";
+option java_outer_classname = "HelloWorldProto";
+
+package helloworld;
+
+// The greeting service definition.
+service GreeterService {
+    // Sends a greeting
+    rpc SayHello (HelloRequest) returns (HelloReply) {}
+
+    rpc ItKeepsTalking (stream HelloRequest) returns (HelloReply) {}
+
+    rpc ItKeepsReplying (HelloRequest) returns (stream HelloReply) {}
+
+    rpc StreamHellos (stream HelloRequest) returns (stream HelloReply) {}
+}
+
+// The request message containing the user's name.
+message HelloRequest {
+    string name = 1;
+}
+
+// The response message containing the greetings
+message HelloReply {
+    string message = 1;
+}

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/src/test/protobuf/echo_service.proto
@@ -1,0 +1,13 @@
+syntax = "proto3";
+
+option java_multiple_files = true;
+option java_package = "example.myapp.echo.grpc";
+
+package echo;
+
+import "echo_message.proto";
+
+// The greeting service definition.
+service EchoService {
+    rpc Echo (EchoMessage) returns (EchoMessage) {}
+}

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/src/test/scala/example/myapp/echo/EchoServiceImpl.scala
@@ -1,0 +1,9 @@
+package example.myapp.echo
+
+import scala.concurrent.Future
+
+import example.myapp.echo.grpc._
+
+class EchoServiceImpl extends EchoService {
+  def echo(in: EchoMessage): Future[EchoMessage] = Future.successful(in)
+}

sbt-plugin/src/sbt-test/gen-scala-server/03-test-config/test
@@ -1,0 +1,15 @@
+> test:compile
+
+# the Scala server was generated in Compile
+$ exists target/scala-2.12/src_managed/main/example/myapp/helloworld/grpc/GreeterServiceHandler.scala
+
+# only the Scala client was generated in Test
+-$ exists target/scala-2.12/src_managed/test/example/myapp/helloworld/grpc/GreeterServiceHandler.scala
+$ exists target/scala-2.12/src_managed/test/example/myapp/helloworld/grpc/GreeterServiceClient.scala
+
+> it:protocGenerate
+
+# only the Java server was generated in IntegrationTest
+-$ exists target/scala-2.12/src_managed/it/example/myapp/helloworld/grpc/GreeterServiceHandler.scala
+-$ exists target/scala-2.12/src_managed/it/example/myapp/helloworld/grpc/GreeterServiceClient.scala
+$ exists target/scala-2.12/src_managed/it/example/myapp/helloworld/grpc/GreeterServiceHandlerFactory.java