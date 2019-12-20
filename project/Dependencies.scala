0bc78f5e417fb5ef544e7516c4a3adf535a60307
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-12-20 08:47:04 +0100

    Update slf4j-api to 1.7.30 (#771)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     // Details are in https://github.com/akka/akka-grpc/pull/469
     val grpcInteropTesting = ("io.grpc" % "grpc-interop-testing" % Versions.grpc).exclude("io.grpc", "grpc-alts")
 
-    val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.29"
+    val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
     val mavenPluginApi = "org.apache.maven" % "maven-plugin-api" % Versions.maven // Apache v2
     val mavenCore = "org.apache.maven" % "maven-core" % Versions.maven // Apache v2
     val protocJar = "com.github.os72" % "protoc-jar" % "3.11.1"
