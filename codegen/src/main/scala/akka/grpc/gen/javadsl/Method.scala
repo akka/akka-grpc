c94bea117b1a8e003ba4e9b95a360da02bf4fd3e
Tim Whittington <tim.whittington@indexity.io>
2020-02-20 14:00:35 +1300

    Codegen support for grpc-web
    Adapt code generation to use new protocol negotiation and marshallers, adding support for grpc-web and grpc-web-text variants.

--- codegen/src/main/scala/akka/grpc/gen/javadsl/Method.scala
+++ codegen/src/main/scala/akka/grpc/gen/javadsl/Method.scala
@@ -28,8 +28,8 @@ final case class Method(
     else "GrpcMarshalling.unmarshal"
 
   def marshal =
-    if (outputStreaming) "GrpcMarshalling.marshalStream"
-    else "GrpcMarshalling.marshal"
+    if (outputStreaming) "GrpcMarshalling.marshalStream2"
+    else "GrpcMarshalling.marshal2"
 
   def inputTypeUnboxed = getMessageType(inputType)
   def outputTypeUnboxed = getMessageType(outputType)
