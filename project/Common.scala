1db0cf120afa594580f0319e3cfd9d296b12e4f3
Arnout Engelen <arnout@bzzt.net>
2020-05-11 13:57:03 +0000

    Update to latest master

--- project/Common.scala
+++ project/Common.scala
@@ -20,6 +20,7 @@ object Common extends AutoPlugin {
       resolvers += Resolver.bintrayRepo("akka", "snapshots"),
       homepage := Some(url("https://akka.io/")),
       scmInfo := Some(ScmInfo(url("https://github.com/akka/akka-grpc"), "git@github.com:akka/akka-grpc")),
+      resolvers += Resolver.bintrayRepo("akka", "snapshots"),
       developers += Developer(
           "contributors",
           "Contributors",
