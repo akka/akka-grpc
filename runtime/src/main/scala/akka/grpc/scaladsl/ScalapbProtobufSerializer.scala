c9a38ff3f8d20135927779d331318626994ea281
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-10 13:22:22 +0200

    Update compilerplugin, scalapb-runtime to 0.10.4 (#995)
    * Update compilerplugin, scalapb-runtime to 0.10.4
    
    * Avoid deprecated Message type #997
    
    Co-authored-by: Arnout Engelen <arnout@bzzt.net>

--- runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
+++ runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
@@ -7,12 +7,10 @@ package akka.grpc.scaladsl
 import akka.annotation.ApiMayChange
 import akka.grpc.ProtobufSerializer
 import akka.util.ByteString
-import scalapb.{ GeneratedMessage, GeneratedMessageCompanion, Message }
-import com.github.ghik.silencer.silent
+import scalapb.{ GeneratedMessage, GeneratedMessageCompanion }
 
-@silent("deprecated")
 @ApiMayChange
-class ScalapbProtobufSerializer[T <: GeneratedMessage with Message[T]](companion: GeneratedMessageCompanion[T])
+class ScalapbProtobufSerializer[T <: GeneratedMessage](companion: GeneratedMessageCompanion[T])
     extends ProtobufSerializer[T] {
   override def serialize(t: T) = ByteString(companion.toByteArray(t))
   override def deserialize(bytes: ByteString): T = companion.parseFrom(bytes.iterator.asInputStream)
