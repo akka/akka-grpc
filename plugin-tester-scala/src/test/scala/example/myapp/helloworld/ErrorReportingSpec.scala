92215940b5f059d78fa0f74fb4e98f4878518ba1
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2020-06-29 16:12:07 +0300

    test compilation and run tests with gradle (#1041)
    * fix: test compilation and run tests with gradle
    
    * Add plugintester scalatestplus-junit dependency to sbt build
    
    * scalafmt
    
    * Follow instructions on how to invoke gradle on travis
    
    * chore: rename scala version variable in build.gradle
    
    Co-authored-by: Arnout Engelen <arnout@bzzt.net>

--- plugin-tester-scala/src/test/scala/example/myapp/helloworld/ErrorReportingSpec.scala
+++ plugin-tester-scala/src/test/scala/example/myapp/helloworld/ErrorReportingSpec.scala
@@ -4,35 +4,41 @@
 
 package example.myapp.helloworld
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorSystem, ClassicActorSystemProvider }
 import akka.grpc.internal.GrpcProtocolNative
-import akka.http.scaladsl.{ Http, HttpConnectionContext }
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.HttpEntity.{ Chunked, LastChunk }
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.{ Http, HttpConnectionContext }
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 import example.myapp.helloworld.grpc.{ GreeterService, GreeterServiceHandler }
 import io.grpc.Status
+import org.junit.runner.RunWith
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.time.Span
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.junit.JUnitRunner
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
+@RunWith(classOf[JUnitRunner])
 class ErrorReportingSpec extends AnyWordSpec with Matchers with ScalaFutures with BeforeAndAfterAll {
-  implicit val sys = ActorSystem()
+
   override implicit val patienceConfig = PatienceConfig(5.seconds, Span(100, org.scalatest.time.Millis))
 
+  implicit val system: ActorSystem = ActorSystem()
+
+  implicit val mat = ActorMaterializer()
+
   "A gRPC server" should {
-    implicit val mat = ActorMaterializer()
 
     val binding = Http()
       .bindAndHandleAsync(
-        GreeterServiceHandler(new GreeterServiceImpl()),
+        GreeterServiceHandler(new GreeterServiceImpl())(system.asInstanceOf[ClassicActorSystemProvider]),
         interface = "127.0.0.1",
         port = 0,
         connectionContext = HttpConnectionContext())
@@ -73,5 +79,5 @@ class ErrorReportingSpec extends AnyWordSpec with Matchers with ScalaFutures wit
   }
 
   override def afterAll: Unit =
-    Await.result(sys.terminate(), 5.seconds)
+    Await.result(system.terminate(), 5.seconds)
 }
