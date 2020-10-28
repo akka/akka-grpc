0536b95fa1537b0bf322c0cf25d8bc113ab21ca1
Arnout Engelen <arnout@bzzt.net>
2020-10-28 10:39:51 +0000

    Implement unary calls
    empty_unary and large_unary now work with an Akka HTTP server, but not
    yet with the netty-based server

--- runtime/src/main/scala/akka/grpc/internal/AkkaHttpClientUtils.scala
+++ runtime/src/main/scala/akka/grpc/internal/AkkaHttpClientUtils.scala
@@ -1,19 +1,26 @@
 package akka.grpc.internal
 
+import java.net.InetSocketAddress
+import java.security.SecureRandom
+
 import akka.Done
 import akka.actor.ClassicActorSystemProvider
 import akka.annotation.InternalApi
 import akka.event.LoggingAdapter
-import akka.grpc.{ GrpcClientSettings, GrpcResponseMetadata, GrpcSingleResponse }
+import akka.grpc.{ GrpcClientSettings, GrpcResponseMetadata, GrpcSingleResponse, ProtobufSerializer }
 import akka.http.Http2Bridge
-import akka.http.scaladsl.model.{ AttributeKey, HttpRequest, HttpResponse, RequestResponseAssociation }
+import akka.http.scaladsl.{ ClientTransport, ConnectionContext }
+import akka.http.scaladsl.model.{ AttributeKey, HttpRequest, HttpResponse, RequestResponseAssociation, Uri }
+import akka.http.scaladsl.settings.ClientConnectionSettings
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.{ Flow, Sink, Source }
+import akka.stream.scaladsl.{ Flow, Keep, Sink, Source }
 import akka.util.OptionVal
 import com.github.ghik.silencer.silent
 import io.grpc.{ CallOptions, MethodDescriptor }
+import javax.net.ssl.{ KeyManager, SSLContext, TrustManager }
 
 import scala.concurrent.{ Future, Promise }
+import scala.util.{ Failure, Success }
 
 /**
  * INTERNAL API
@@ -29,18 +36,43 @@ object AkkaHttpClientUtils {
   def createChannel(settings: GrpcClientSettings, log: LoggingAdapter)(
       implicit sys: ClassicActorSystemProvider): InternalChannel = {
     implicit val ec = sys.classicSystem.dispatcher
+    implicit val writer = GrpcProtocolNative.newWriter(Codecs.supportedCodecs.head)
 
-    val donePromise = Promise[Done]()
-    // TODO FIXME discovery/loadbalancing/...?
+    // TODO FIXME discovery, loadbalancing etc
     val host = settings.serviceName
 
-    val queue =
+    val connectionContext = ConnectionContext.httpsClient {
+      settings.trustManager match {
+        case None => SSLContext.getDefault
+        case Some(trustManager) =>
+          val sslContext: SSLContext = SSLContext.getInstance("TLS")
+          sslContext.init(Array[KeyManager](), Array[TrustManager](trustManager), new SecureRandom)
+          sslContext
+      }
+    }
+
+    val clientConnectionSettings = settings.overrideAuthority match {
+      case None => ClientConnectionSettings(sys)
+      case Some(authority) =>
+        ClientConnectionSettings(sys).withTransport(ClientTransport.withCustomResolver((host, port) => {
+          assert(host == authority)
+          assert(port == settings.defaultPort)
+          Future.successful(new InetSocketAddress(settings.serviceName, settings.defaultPort))
+        }))
+    }
+
+    val (queue, doneFuture) =
       Source
         .queue[HttpRequest](4242, OverflowStrategy.fail)
-        .via(Http2Bridge.connect(host, settings.defaultPort))
-        .to(Sink.foreach { res =>
+        .via(
+          Http2Bridge.connect(
+            settings.overrideAuthority.getOrElse(host),
+            settings.defaultPort,
+            clientConnectionSettings,
+            connectionContext))
+        .toMat(Sink.foreach { res =>
           res.attribute(ResponsePromise.Key).get.promise.trySuccess(res)
-        })
+        })(Keep.both)
         .run()
 
     def singleRequest(request: HttpRequest): Future[HttpResponse] = {
@@ -48,15 +80,39 @@ object AkkaHttpClientUtils {
       queue.offer(request.addAttribute(ResponsePromise.Key, ResponsePromise(p))).flatMap(_ => p.future)
     }
 
+    implicit def serializerFromMethodDescriptor[I, O](descriptor: MethodDescriptor[I, O]): ProtobufSerializer[I] =
+      descriptor.getRequestMarshaller.asInstanceOf[Marshaller[_]].u.asInstanceOf[ProtobufSerializer[I]]
+    implicit def deserializerFromMethodDescriptor[I, O](descriptor: MethodDescriptor[I, O]): ProtobufSerializer[O] =
+      descriptor.getResponseMarshaller.asInstanceOf[Marshaller[_]].u.asInstanceOf[ProtobufSerializer[O]]
+
     new InternalChannel() {
-      override def shutdown(): Unit = ???
-      override def done: Future[Done] = donePromise.future
+      override def shutdown(): Unit = queue.complete()
+      override def done: Future[Done] = doneFuture
 
       override def invoke[I, O](
           request: I,
           headers: MetadataImpl,
           descriptor: MethodDescriptor[I, O],
-          options: CallOptions): Future[O] = ???
+          options: CallOptions): Future[O] = {
+        implicit val serializer: ProtobufSerializer[I] = descriptor
+        val deserializer: ProtobufSerializer[O] = descriptor
+        singleRequest(
+          GrpcRequestHelpers(
+            Uri(s"https://${settings.overrideAuthority.getOrElse(host)}/" + descriptor.getFullMethodName),
+            Source.single(request))).flatMap(response => {
+          Codecs.detect(response) match {
+            case Success(codec) =>
+              implicit val reader = GrpcProtocolNative.newReader(codec)
+              response.entity.dataBytes
+                .via(reader.dataFrameDecoder)
+                .map(data => deserializer.deserialize(data))
+                // TODO probably shouldn't cancel after reading the element?
+                .runWith(Sink.head[O])
+            case Failure(e) =>
+              Future.failed(e)
+          }
+        })
+      }
 
       override def invokeWithMetadata[I, O](
           request: I,
--- runtime/src/main/scala/akka/http/Http2Bridge.scala
+++ runtime/src/main/scala/akka/http/Http2Bridge.scala
@@ -2,7 +2,9 @@ package akka.http
 
 import akka.actor.ClassicActorSystemProvider
 import akka.http.impl.engine.http2.Http2
+import akka.http.scaladsl.HttpsConnectionContext
 import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
+import akka.http.scaladsl.settings.ClientConnectionSettings
 import akka.stream.scaladsl.Flow
 
 /**
@@ -10,6 +12,8 @@ import akka.stream.scaladsl.Flow
  * https://github.com/akka/akka-http/pull/3511 and further
  */
 object Http2Bridge {
-  def connect(host: String, port: Int)(implicit sys: ClassicActorSystemProvider): Flow[HttpRequest, HttpResponse, Any] =
-    Http2().outgoingConnection(host, port)
+  def connect(host: String, port: Int, settings: ClientConnectionSettings, connectionContext: HttpsConnectionContext)(
+      implicit sys: ClassicActorSystemProvider): Flow[HttpRequest, HttpResponse, Any] = {
+    Http2().outgoingConnection(host, port, settings, connectionContext)
+  }
 }
