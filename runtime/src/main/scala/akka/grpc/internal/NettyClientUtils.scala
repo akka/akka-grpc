cce0a1db8d4c221d053e7987b1b336169c54f148
Arnout Engelen <arnout@bzzt.net>
2020-10-28 16:26:04 +0000

    Implement streaming requests with the Akka HTTP backend
    Removed some optimization to make the API fit better

--- runtime/src/main/scala/akka/grpc/internal/NettyClientUtils.scala
+++ runtime/src/main/scala/akka/grpc/internal/NettyClientUtils.scala
@@ -7,12 +7,11 @@ package akka.grpc.internal
 import java.util.concurrent.TimeUnit
 
 import javax.net.ssl.SSLContext
-import akka.Done
+import akka.{ Done, NotUsed }
 import akka.annotation.InternalApi
 import akka.event.LoggingAdapter
 import akka.grpc.{ GrpcClientSettings, GrpcResponseMetadata, GrpcSingleResponse }
 import akka.stream.scaladsl.{ Flow, Keep, Source }
-import akka.util.OptionVal
 import com.github.ghik.silencer.silent
 import io.grpc.{ CallOptions, MethodDescriptor }
 import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts
@@ -144,22 +143,19 @@ object NettyClientUtils {
       }
 
       override def invokeWithMetadata[I, O](
-          source: I,
-          defaultFlow: OptionVal[Flow[I, O, Future[GrpcResponseMetadata]]],
+          source: Source[I, NotUsed],
           fqMethodName: String,
           headers: MetadataImpl,
           descriptor: MethodDescriptor[I, O],
+          streamingResponse: Boolean,
           options: CallOptions) = {
-        val flow = defaultFlow match {
-          case OptionVal.Some(f) => f
-          case OptionVal.None =>
-            createFlow(fqMethodName, headers, descriptor, true, callOptionsWithDeadline(options, settings))
-        }
-        Source.single(source).viaMat(flow)(Keep.right)
+        val flow =
+          createFlow(fqMethodName, headers, descriptor, streamingResponse, callOptionsWithDeadline(options, settings))
+        source.viaMat(flow)(Keep.right)
       }
 
       // TODO can't you derive the method name from the descriptor?
-      override def createFlow[I, O](
+      private def createFlow[I, O](
           fqMethodName: String,
           headers: MetadataImpl,
           descriptor: MethodDescriptor[I, O],
