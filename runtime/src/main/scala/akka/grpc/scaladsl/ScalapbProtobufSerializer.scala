27f41ee3d1b81b3b9609169ec81caf0f2e064367
carbonshow <wenzhitao515@qq.com>
2020-11-05 18:13:39 +0800

    There are 2 ways to parse akka.util.ByteString in protobuf: 1. put all the fragments into one compact continuous memory, then protobuf parses without copy 2. just pass InputStream, protobuf does copy while parsing if it has to. According to the test result, the first way performs better for almost all the situations. Maybe large memory fragment copy gets a better optimization.
    see: https://github.com/akka/akka-grpc/issues/1163

--- runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
+++ runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
@@ -7,11 +7,13 @@ package akka.grpc.scaladsl
 import akka.annotation.ApiMayChange
 import akka.grpc.ProtobufSerializer
 import akka.util.ByteString
+import com.google.protobuf.CodedInputStream
 import scalapb.{ GeneratedMessage, GeneratedMessageCompanion }
 
 @ApiMayChange
 class ScalapbProtobufSerializer[T <: GeneratedMessage](companion: GeneratedMessageCompanion[T])
     extends ProtobufSerializer[T] {
-  override def serialize(t: T) = ByteString(companion.toByteArray(t))
-  override def deserialize(bytes: ByteString): T = companion.parseFrom(bytes.iterator.asInputStream)
+  override def serialize(t: T): ByteString = ByteString(companion.toByteArray(t))
+  override def deserialize(bytes: ByteString): T =
+    companion.parseFrom(CodedInputStream.newInstance(bytes.asByteBuffer))
 }
