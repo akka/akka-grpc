12a58a65b2effd51c57ea0f68412c893acc32271
Arnout Engelen <arnout@bzzt.net>
2020-11-06 10:51:32 +0000

    Update Akka HTTP, process trailing (status) headers

--- runtime/src/main/scala/akka/http/Http2Bridge.scala
+++ runtime/src/main/scala/akka/http/Http2Bridge.scala
@@ -5,6 +5,7 @@
 package akka.http
 
 import akka.actor.ClassicActorSystemProvider
+import akka.event.LoggingAdapter
 import akka.http.impl.engine.http2.Http2
 import akka.http.scaladsl.HttpsConnectionContext
 import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
@@ -16,8 +17,12 @@ import akka.stream.scaladsl.Flow
  * https://github.com/akka/akka-http/pull/3511 and further
  */
 object Http2Bridge {
-  def connect(host: String, port: Int, settings: ClientConnectionSettings, connectionContext: HttpsConnectionContext)(
-      implicit sys: ClassicActorSystemProvider): Flow[HttpRequest, HttpResponse, Any] = {
-    Http2().outgoingConnection(host, port, settings, connectionContext)
+  def connect(
+      host: String,
+      port: Int,
+      connectionContext: HttpsConnectionContext,
+      settings: ClientConnectionSettings,
+      log: LoggingAdapter)(implicit sys: ClassicActorSystemProvider): Flow[HttpRequest, HttpResponse, Any] = {
+    Http2().outgoingConnection(host, port, connectionContext, settings, log)
   }
 }
