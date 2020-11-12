6472faf96d10855ba76a1f0f2b124ff6a4868d4a
Arnout Engelen <arnout@bzzt.net>
2020-11-12 09:40:02 +0100

    Avoid deadlock (#1178)
    * Avoid deadlock
    
    The 'concat' is used to make sure the stream fails with the right error
    when the trailing headers contain an error status code. To avoid
    deadlock we should put that after the watchTermination that successfully
    terminates the stream where there are no trailing headers at all.
    
    * Update Akka HTTP
    
    To fix the remaining timeouts caused by https://github.com/akka/akka-http/pull/3619

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     // https://doc.akka.io//docs/akka/current/project/downstream-upgrade-strategy.html
     val akka = "2.6.9"
     val akkaBinary = "2.6"
-    val akkaHttp = "10.2.1+92-764faf46"
+    val akkaHttp = "10.2.1+103-664568a0"
     val akkaHttpBinary = "10.2"
 
     val grpc = "1.33.1" // checked synced by GrpcVersionSyncCheckPlugin
