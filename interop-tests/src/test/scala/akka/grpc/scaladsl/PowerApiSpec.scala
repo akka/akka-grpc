b6a31db520ab5cba26d4df4bae32621d3ae3b882
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-11-27 10:10:00 +0100

    Test future for streaming headers redeems eagerly (#1207)
    * Test future for streaming headers redeems eagerly
    
    * Cleanup, simplify

--- interop-tests/src/test/scala/akka/grpc/scaladsl/PowerApiSpec.scala
+++ interop-tests/src/test/scala/akka/grpc/scaladsl/PowerApiSpec.scala
@@ -4,19 +4,34 @@
 
 package akka.grpc.scaladsl
 
-import com.typesafe.config.ConfigFactory
-
+import scala.concurrent.Future
+import scala.concurrent.Promise
 import scala.concurrent.duration._
+
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.grpc.GrpcClientSettings
-import akka.grpc.internal.{ GrpcEntityHelpers, GrpcProtocolNative, GrpcResponseHelpers, HeaderMetadataImpl, Identity }
+import akka.grpc.GrpcResponseMetadata
+import akka.grpc.internal.GrpcEntityHelpers
+import akka.grpc.internal.GrpcProtocolNative
+import akka.grpc.internal.GrpcResponseHelpers
+import akka.grpc.internal.HeaderMetadataImpl
+import akka.grpc.internal.Identity
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives
+import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
 import akka.testkit.TestKit
+import com.typesafe.config.ConfigFactory
+import example.myapp.helloworld.grpc.helloworld.GreeterServiceClient
+import example.myapp.helloworld.grpc.helloworld.GreeterServicePowerApiHandler
+import example.myapp.helloworld.grpc.helloworld.HelloReply
+import example.myapp.helloworld.grpc.helloworld.HelloRequest
 import example.myapp.helloworld.grpc.helloworld._
 import io.grpc.Status
+import org.scalatest.BeforeAndAfter
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.time.Span
@@ -25,23 +40,37 @@ import org.scalatest.wordspec.AnyWordSpecLike
 class PowerApiSpecNetty extends PowerApiSpec("netty")
 class PowerApiSpecAkkaHttp extends PowerApiSpec("akka-http")
 
-class PowerApiSpec(backend: String)
+abstract class PowerApiSpec(backend: String)
     extends TestKit(ActorSystem(
       "GrpcExceptionHandlerSpec",
       ConfigFactory.parseString(s"""akka.grpc.client."*".backend = "$backend" """).withFallback(ConfigFactory.load())))
     with AnyWordSpecLike
     with Matchers
     with ScalaFutures
-    with Directives {
+    with Directives
+    with BeforeAndAfter
+    with BeforeAndAfterAll {
 
   override implicit val patienceConfig = PatienceConfig(5.seconds, Span(10, org.scalatest.time.Millis))
 
   val server =
     Http().newServerAt("localhost", 0).bind(GreeterServicePowerApiHandler(new PowerGreeterServiceImpl())).futureValue
 
+  var client: GreeterServiceClient = _
+
+  after {
+    if (client != null && !client.closed.isCompleted) {
+      client.close().futureValue
+    }
+  }
+  override protected def afterAll(): Unit = {
+    server.terminate(3.seconds)
+    super.afterAll()
+  }
+
   "The power API" should {
     "successfully pass metadata from client to server" in {
-      val client = GreeterServiceClient(
+      client = GreeterServiceClient(
         GrpcClientSettings.connectToServiceAt("localhost", server.localAddress.getPort).withTls(false))
 
       client
@@ -71,7 +100,7 @@ class PowerApiSpec(backend: String)
           })
           .futureValue
 
-      val client = GreeterServiceClient(
+      client = GreeterServiceClient(
         GrpcClientSettings.connectToServiceAt("localhost", specialServer.localAddress.getPort).withTls(false))
 
       val response = client
@@ -84,5 +113,33 @@ class PowerApiSpec(backend: String)
       response.headers.getText("baz").get should be("qux")
       response.trailers.futureValue.getText("foo").get should be("bar")
     }
+
+    "(on streamed calls) redeem the headers future as soon as they're available (and trailers future when trailers arrive)" in {
+
+      // invoking streamed calls using the power API materializes a Future[GrpcResponseMetadata]
+      // that should redeem as soon as the HEADERS is consumed. Then, the GrpcResponseMetadata instance
+      // contains another Future that will redeem when receiving the trailers.
+      client = GreeterServiceClient(
+        GrpcClientSettings.connectToServiceAt("localhost", server.localAddress.getPort).withTls(false))
+
+      val p = Promise[HelloRequest]()
+      val requests: Source[HelloRequest, NotUsed] = Source.single(HelloRequest("Alice")).concat(Source.future(p.future))
+
+      val responseSource: Source[HelloReply, Future[GrpcResponseMetadata]] =
+        client.streamHellos().invokeWithMetadata(requests)
+
+      val headers: Future[GrpcResponseMetadata] = responseSource.to(Sink.ignore).run()
+
+      // blocks progress until redeeming `headers`
+      val trailers = headers.futureValue.trailers
+
+      // Don't send the finalization message until the headers future was redeemed (see above)
+      trailers.isCompleted should be(false)
+      p.trySuccess(HelloRequest("ByeBye"))
+
+      trailers.futureValue // the trailers future eventually completes
+
+    }
   }
+
 }
