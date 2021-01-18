9532bdd8835678903b8678c9a882f4f5beef4b34
Arnout Engelen <arnout@bzzt.net>
2021-01-18 09:16:57 +0100

    Telemetry hook (#1233)
    * Telemetry hook
    
    * Also add to generated Java server code
    
    * allow the spi to modify the request
    
    * more reliable test
    
    * Add copyright headers
    
    * private/internal markers

---
+++ interop-tests/src/test/scala/akka/grpc/scaladsl/TelemetrySpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.grpc.scaladsl
+
+import com.typesafe.config.ConfigFactory
+
+import akka.actor.ActorSystem
+import akka.grpc.internal.{ GrpcProtocolNative, GrpcRequestHelpers, Identity, TelemetryExtension, TelemetrySpi }
+import akka.http.javadsl.model.HttpRequest
+import akka.stream.scaladsl.Source
+import akka.testkit.TestKit
+
+import example.myapp.helloworld.grpc.helloworld.{ GreeterService, GreeterServiceHandler, HelloRequest }
+
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TelemetrySpec
+    extends TestKit(
+      ActorSystem(
+        "TelemetrySpec",
+        ConfigFactory
+          .parseString(s"""akka.grpc.telemetry-class = "akka.grpc.scaladsl.CollectingTelemetrySpi" """)
+          .withFallback(ConfigFactory.load())))
+    with AnyWordSpecLike
+    with Matchers
+    with ScalaFutures {
+  "The client-side telemetry hook" should {
+    "pick up matched requests" in {
+      val handler = GreeterServiceHandler(new CountingGreeterServiceImpl)
+      implicit val ser = GreeterService.Serializers.HelloRequestSerializer
+      implicit val writer = GrpcProtocolNative.newWriter(Identity)
+      handler(
+        GrpcRequestHelpers(
+          s"https://localhost/${GreeterService.name}/SayHello",
+          Nil,
+          Source.single(HelloRequest("Joe")))).futureValue
+
+      val spi = TelemetryExtension(system).spi.asInstanceOf[CollectingTelemetrySpi]
+      spi.requests.size should be(1)
+      val (prefix, method, request) = spi.requests(0)
+      prefix should be(GreeterService.name)
+      method should be("SayHello")
+      request.entity.getContentType should be(GrpcProtocolNative.contentType)
+    }
+  }
+}
+
+class CollectingTelemetrySpi extends TelemetrySpi {
+  @volatile
+  var requests: List[(String, String, HttpRequest)] = Nil
+
+  override def onRequest[T <: HttpRequest](prefix: String, method: String, request: T): T = {
+    requests :+= (prefix, method, request)
+    request
+  }
+}
---
+++ runtime/src/main/scala/akka/grpc/internal/TelemetrySpi.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.grpc.internal
+
+import akka.actor.{
+  ActorSystem,
+  ClassicActorSystemProvider,
+  ExtendedActorSystem,
+  Extension,
+  ExtensionId,
+  ExtensionIdProvider
+}
+import akka.annotation.{ InternalApi, InternalStableApi }
+import akka.http.javadsl.model.HttpRequest
+
+import scala.annotation.nowarn
+
+/** INTERNAL API */
+@InternalStableApi
+private[internal] class TelemetryExtensionImpl(val spi: TelemetrySpi) extends Extension
+
+/** INTERNAL API */
+@InternalStableApi
+object TelemetryExtension extends ExtensionId[TelemetryExtensionImpl] with ExtensionIdProvider {
+  override def lookup = TelemetryExtension
+  override def createExtension(system: ExtendedActorSystem) =
+    new TelemetryExtensionImpl(TelemetrySpi(system))
+
+  /** Java API */
+  override def get(system: ActorSystem): TelemetryExtensionImpl = super.get(system)
+  override def get(system: ClassicActorSystemProvider): TelemetryExtensionImpl = super.get(system)
+}
+
+private[internal] object TelemetrySpi {
+  private val ConfigKey = "akka.grpc.telemetry-class"
+  def apply(system: ClassicActorSystemProvider): TelemetrySpi = {
+    if (!system.classicSystem.settings.config.hasPath(ConfigKey)) NoOpTelemetry
+    else {
+      val fqcn = system.classicSystem.settings.config.getString(ConfigKey)
+      try {
+        system.classicSystem
+          .asInstanceOf[ExtendedActorSystem]
+          .dynamicAccess
+          .createInstanceFor[TelemetrySpi](fqcn, Nil)
+          .get
+      } catch {
+        case ex: Throwable =>
+          system.classicSystem.log.debug(
+            "{} references a class that could not be instantiated ({}) falling back to no-op implementation",
+            fqcn,
+            ex.toString)
+          NoOpTelemetry
+      }
+    }
+  }
+}
+
+@InternalStableApi
+trait TelemetrySpi {
+  @nowarn
+  def onRequest[T <: HttpRequest](prefix: String, method: String, request: T): T = request
+}
+
+@InternalApi
+private[internal] object NoOpTelemetry extends TelemetrySpi
