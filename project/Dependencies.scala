1dd3a194b68721c517f40af917b68f07d47d6cce
Arnout Engelen <arnout@bzzt.net>
2021-02-27 20:49:34 +0100

    Update scala versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import buildinfo.BuildInfo
 
 object Dependencies {
   object Versions {
-    val scala212 = "2.12.11"
+    val scala212 = "2.12.13"
     val scala213 = "2.13.5"
 
     // the order in the list is important because the head will be considered the default.
