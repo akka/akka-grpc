99cce057c9cc9dfd614e0e8380e5dfa851705828
Scala Steward <me@scala-steward.org>
2021-03-12 00:03:17 +0100

    Update protobuf-java to 3.15.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   }
 
   object Protobuf {
-    val googleCommonProtos = "com.google.protobuf" % "protobuf-java" % "3.15.5" % "protobuf"
+    val googleCommonProtos = "com.google.protobuf" % "protobuf-java" % "3.15.6" % "protobuf"
   }
 
   object Plugins {
