85086a77f3df62bfe93ea47d4d64410e4167ba85
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-13 08:51:26 +0200

    Update protobuf-java to 3.17.0 (#1342)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
     val grpc = "1.37.0" // checked synced by GrpcVersionSyncCheckPlugin
     // Even referenced explicitly in the sbt-plugin's sbt-tests
-    val googleProtobuf = "3.16.0"
+    val googleProtobuf = "3.17.0"
 
     val scalaTest = "3.1.4"
 
