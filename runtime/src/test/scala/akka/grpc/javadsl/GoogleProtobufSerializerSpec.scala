95c4f4822d3e91661af79e6d0c7e759d31800e95
Arnout Engelen <arnout@bzzt.net>
2021-06-01 14:55:11 +0200

    Add protobuf serializer tests
    To easily exercise the code and verify the behavior

---
+++ runtime/src/test/scala/akka/grpc/javadsl/GoogleProtobufSerializerSpec.scala
@@ -0,0 +1,18 @@
+package akka.grpc.javadsl
+
+import com.google.protobuf.{ ByteString, Any => ProtobufAny }
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class GoogleProtobufSerializerSpec extends AnyWordSpec with Matchers {
+  "Google protobuf serializer" should {
+    "successfully serialize and deserialize a protobuf Any object" in {
+      val anySerializer = new GoogleProtobufSerializer(ProtobufAny.parser())
+
+      val obj = ProtobufAny.newBuilder().setTypeUrl("asdf").setValue(ByteString.copyFromUtf8("ASDF")).build()
+      val serialized = anySerializer.serialize(obj)
+      val deserialized = anySerializer.deserialize(serialized)
+      deserialized should be(obj)
+    }
+  }
+}
---
+++ runtime/src/test/scala/akka/grpc/scaladsl/ScalapbProtobufSerializerSpec.scala
@@ -0,0 +1,19 @@
+package akka.grpc.scaladsl
+
+import com.google.protobuf.any.{Any => ScalapbAny}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import com.google.protobuf.ByteString
+
+class ScalapbProtobufSerializerSpec extends AnyWordSpec with Matchers {
+  "Google protobuf serializer" should {
+    "successfully serialize and deserialize a protobuf Any object" in {
+      val anySerializer = new ScalapbProtobufSerializer(ScalapbAny)
+
+      val obj = ScalapbAny("asdf", ByteString.copyFromUtf8("ASDF"))
+      val serialized = anySerializer.serialize(obj)
+      val deserialized = anySerializer.deserialize(serialized)
+      deserialized should be(obj)
+    }
+  }
+}
