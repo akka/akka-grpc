1aaea7f8b425e82966fde8b1a3051299fa3475c4
Viktor Klang (√) <viktor.klang@gmail.com>
2021-06-02 08:16:04 +0000

    Avoids copying in BaseMarshalling when first read is total (#1366)
    * Avoids copying in BaseMarshalling when first read is total
    
    * Optimizing for KnownLength InputStreams in Mashalling
    
    * Adding comment explaining trade-off made in BaseMarshaller

--- runtime/src/main/scala/akka/grpc/internal/Marshallers.scala
+++ runtime/src/main/scala/akka/grpc/internal/Marshallers.scala
@@ -18,19 +18,35 @@ abstract class BaseMarshaller[T](val protobufSerializer: ProtobufSerializer[T])
     extends io.grpc.MethodDescriptor.Marshaller[T]
     with WithProtobufSerializer[T] {
   override def parse(stream: InputStream): T = {
-    val baos = new ByteArrayOutputStream(math.max(64, stream.available()))
-    val buffer = new Array[Byte](32 * 1024)
+    val buffer =
+      new Array[Byte](stream match {
+        case k: KnownLength => math.max(0, k.available()) // No need to oversize this if we already know the size
+        case _              => 32 * 1024
+      })
 
     // Blocking calls underneath...
     // we can't avoid it for the moment because we are relying on the Netty's Channel API
-    var bytesRead = stream.read(buffer)
-    while (bytesRead >= 0) {
-      baos.write(buffer, 0, bytesRead)
-      bytesRead = stream.read(buffer)
-    }
-    protobufSerializer.deserialize(akka.util.ByteString.fromArrayUnsafe(baos.toByteArray))
-  }
+    val bytes = stream.read(buffer, 0, buffer.length) match {
+      case -1 =>
+        akka.util.ByteString.empty // EOF immediately
+      case n if n < buffer.length =>
+        // WARNING: buffer is retained in full below,
+        // which could be problematic if ProtobufSerializer.deserialize keeps a reference to the ByteString
+        akka.util.ByteString.fromArrayUnsafe(buffer, 0, n)
+      case full =>
+        val baos = new ByteArrayOutputStream(buffer.length * 2) // To avoid immediate resize
+        var bytesRead = full
 
+        do {
+          baos.write(buffer, 0, bytesRead)
+          bytesRead = stream.read(buffer)
+        } while (bytesRead >= 0)
+
+        akka.util.ByteString.fromArrayUnsafe(baos.toByteArray)
+    }
+
+    protobufSerializer.deserialize(bytes)
+  }
 }
 
 /**
