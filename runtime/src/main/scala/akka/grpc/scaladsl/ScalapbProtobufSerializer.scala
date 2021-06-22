afd0f8f6ab5ba41f4ee209c51b4bc71943b84967
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-06-22 16:07:43 +0200

    use direct buffer

--- runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
+++ runtime/src/main/scala/akka/grpc/scaladsl/ScalapbProtobufSerializer.scala
@@ -6,15 +6,24 @@ package akka.grpc.scaladsl
 
 import akka.annotation.ApiMayChange
 import akka.grpc.ProtobufSerializer
+import akka.io.DirectByteBufferPool
 import akka.util.ByteString
 import com.google.protobuf.CodedInputStream
 import scalapb.{ GeneratedMessage, GeneratedMessageCompanion }
+
+import java.nio.ByteBuffer
 
 @ApiMayChange
 class ScalapbProtobufSerializer[T <: GeneratedMessage](companion: GeneratedMessageCompanion[T])
     extends ProtobufSerializer[T] {
   override def serialize(t: T): ByteString =
     ByteString.fromArrayUnsafe(t.toByteArray)
-  override def deserialize(bytes: ByteString): T =
-    companion.parseFrom(CodedInputStream.newInstance(bytes.asByteBuffer))
+  override def deserialize(bytes: ByteString): T = {
+    val buffer = ByteBuffer.allocateDirect(bytes.length)
+    try {
+      bytes.copyToBuffer(buffer)
+      buffer.flip()
+      companion.parseFrom(CodedInputStream.newInstance(buffer))
+    } finally DirectByteBufferPool.tryCleanDirectByteBuffer(buffer)
+  }
 }
