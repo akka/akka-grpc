df9da42c1af02de0eea87c668d6ced92c4544e85
Arnout Engelen <arnout@bzzt.net>
2021-07-12 12:05:10 +0200

    Remove some braces, just for fun

--- sbt-plugin/src/sbt-test/scala3/01-basic-client-server/src/main/scala/example/myapp/helloworld/GreeterServiceImpl.scala
+++ sbt-plugin/src/sbt-test/scala3/01-basic-client-server/src/main/scala/example/myapp/helloworld/GreeterServiceImpl.scala
@@ -7,13 +7,16 @@ import akka.stream.scaladsl.Source
 
 import example.myapp.helloworld.grpc._
 
-class GreeterServiceImpl extends GreeterService {
-  override def sayHello(in: HelloRequest): Future[HelloReply] = Future.successful(HelloReply(s"Hello, ${in.name}!"))
+class GreeterServiceImpl extends GreeterService:
 
-  override def streamHellos(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] = ???
+  override def sayHello(in: HelloRequest): Future[HelloReply] =
+    Future.successful(HelloReply(s"Hello, ${in.name}!"))
 
-  override def itKeepsTalking(in: Source[HelloRequest, NotUsed]): Future[HelloReply] = ???
+  override def streamHellos(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] =
+    ???
 
-  override def itKeepsReplying(in: HelloRequest): Source[HelloReply, NotUsed] = ???
+  override def itKeepsTalking(in: Source[HelloRequest, NotUsed]): Future[HelloReply] =
+    ???
 
-}
+  override def itKeepsReplying(in: HelloRequest): Source[HelloReply, NotUsed] =
+    ???
