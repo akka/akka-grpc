akka/akka-grpc

commit e89e3ded3ceaea11862d58f7db30764ef2b11cd5

build: use latest versions of Scala

project/Common.scala
@@ -1,3 +1,5 @@
+package akka.grpc
+
 import sbt.Keys._
 import sbt._
 import sbt.plugins.JvmPlugin
@@ -31,7 +33,6 @@ object Common extends AutoPlugin {
       licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0")),
       description := "Akka gRPC - Support for building streaming gRPC servers and clients on top of Akka Streams.")
 
-  val silencerVersion = "1.7.5"
   override lazy val projectSettings = Seq(
     projectInfoVersion := (if (isSnapshot.value) "snapshot" else version.value),
     sonatypeProfileName := "com.lightbend",
@@ -74,8 +75,8 @@ object Common extends AutoPlugin {
     Compile / doc / scalacOptions -= "-Xfatal-warnings",
     apiURL := Some(url(s"https://doc.akka.io/api/akka-grpc/${projectInfoVersion.value}/akka/grpc/index.html")),
     libraryDependencies ++= Seq(
-      compilerPlugin(("com.github.ghik" % "silencer-plugin" % silencerVersion).cross(CrossVersion.full)),
-      ("com.github.ghik" % "silencer-lib" % silencerVersion % Provided).cross(CrossVersion.full)),
+      compilerPlugin(("com.github.ghik" % "silencer-plugin" % Dependencies.Versions.silencer).cross(CrossVersion.full)),
+      ("com.github.ghik" % "silencer-lib" % Dependencies.Versions.silencer % Provided).cross(CrossVersion.full)),
     (Test / testOptions) += Tests.Argument(TestFrameworks.ScalaTest, "-oDF"),
     crossScalaVersions := Seq(scala212, scala213),
     mimaReportSignatureProblems := true,

build.sbt
@@ -36,7 +36,7 @@ lazy val codegen = Project(id = akkaGrpcCodegenId, base = file("codegen"))
       art.withClassifier(Some("assembly"))
     },
     (assembly / mainClass) := Some("akka.grpc.gen.Main"),
-    (assembly / assemblyOption) := (assembly / assemblyOption).value.copy(prependShellScript =
+    (assembly / assemblyOption) := (assembly / assemblyOption).value.withPrependShellScript(
       Some(sbtassembly.AssemblyPlugin.defaultUniversalScript(shebang = true))),
     crossScalaVersions := Dependencies.Versions.CrossScalaForPlugin,
     scalaVersion := scala212)
@@ -75,7 +75,7 @@ lazy val scalapbProtocPlugin = Project(id = akkaGrpcProtocPluginId, base = file(
       art.withClassifier(Some("assembly"))
     },
     (assembly / mainClass) := Some("akka.grpc.scalapb.Main"),
-    (assembly / assemblyOption) := (assembly / assemblyOption).value.copy(prependShellScript =
+    (assembly / assemblyOption) := (assembly / assemblyOption).value.withPrependShellScript(
       Some(sbtassembly.AssemblyPlugin.defaultUniversalScript(shebang = true))))
   .settings(
     crossScalaVersions := Dependencies.Versions.CrossScalaForPlugin,

project/Dependencies.scala
@@ -7,7 +7,7 @@ import buildinfo.BuildInfo
 object Dependencies {
   object Versions {
     val scala212 = "2.12.14"
-    val scala213 = "2.13.3"
+    val scala213 = "2.13.6"
 
     // the order in the list is important because the head will be considered the default.
     val CrossScalaForLib = Seq(scala212, scala213)
@@ -28,6 +28,8 @@ object Dependencies {
     val scalaTest = "3.1.4"
 
     val maven = "3.8.1"
+
+    val silencer = "1.7.5"
   }
 
   object Compile {

commit e97a9114c993b49b748128a6a90076a7999aebcf

Fix sonatype profile name
This broke in e89e3ded3ceaea11862d58f7db30764ef2b11cd5, probably because
putting `Common` in a proper package changed the ordering and caused
the default `sonatypeProfileName` to take precendence over our custom
one. Specifying the dependency between the two plugins seems to make it
work again.

project/Common.scala
@@ -8,12 +8,13 @@ import com.lightbend.paradox.projectinfo.ParadoxProjectInfoPluginKeys.projectInf
 import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
 import com.typesafe.tools.mima.plugin.MimaKeys._
 import sbtprotoc.ProtocPlugin.autoImport.PB
+import xerial.sbt.Sonatype
 import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
 
 object Common extends AutoPlugin {
   override def trigger = allRequirements
 
-  override def requires = JvmPlugin
+  override def requires = JvmPlugin && Sonatype
 
   private val consoleDisabledOptions = Seq("-Xfatal-warnings", "-Ywarn-unused", "-Ywarn-unused-import")
 