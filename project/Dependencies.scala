729982e88dc9f34e6ec1d800c45ed2e8f1256cf0
Scala Steward <me@scala-steward.org>
2021-11-17 12:03:55 +0100

    Update protobuf-java to 3.19.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,9 +21,11 @@ object Dependencies {
     val akkaHttp = "10.2.7"
     val akkaHttpBinary = "10.2"
 
-    val grpc = "1.42.0" // checked synced by VersionSyncCheckPlugin
-
-    val googleProtobuf = "3.19.1" // checked synced by VersionSyncCheckPlugin
+    val grpc = "1.42.0" // checked synced by GrpcVersionSyncCheckPlugin
+    // Even referenced explicitly in the sbt-plugin's sbt-tests
+    // If changing this, remember to update protoc plugin version to align in
+    // maven-plugin/src/main/maven/plugin.xml and akka.grpc.sbt.AkkaGrpcPlugin
+    val googleProtobuf = "3.19.1"
 
     val scalaTest = "3.1.4"
 
