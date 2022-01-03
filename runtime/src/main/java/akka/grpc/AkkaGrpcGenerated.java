6a2cda4ba06e0c68ba5bc7f3812d5318e587969a
Sebastian Alfers <sebastian.alfers@gmail.com>
2022-01-03 20:53:40 +0100

    annotation

--- runtime/src/main/java/akka/grpc/AkkaGrpcGenerated.java
+++ runtime/src/main/java/akka/grpc/AkkaGrpcGenerated.java
@@ -1,3 +1,7 @@
 package akka.grpc;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
 public @interface AkkaGrpcGenerated { }
