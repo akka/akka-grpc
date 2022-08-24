f9b768bb229d23783b4a16cbab3a0a48b0e63fba
Sebastian Alfers <sebastian.alfers@gmail.com>
2022-08-24 13:56:57 +0200

    Simplify Rich error model api (#1540)
    use GrpcServiceException and push rich error into its metadata
    
    capture raw Metadata only in RichGrpcMetadataImpl
    
    * Revert the client-side changes so the server-side can be merged
    
    client-side changes can be restored as a separate PR later
    
    Co-authored-by: Arnout Engelen <arnout@bzzt.net>

---
+++ interop-tests/src/test/java/example/myapp/helloworld/grpc/RichErrorNativeImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package example.myapp.helloworld.grpc;
+
+import akka.NotUsed;
+import akka.grpc.GrpcServiceException;
+import akka.stream.javadsl.Source;
+import com.google.rpc.Code;
+import com.google.rpc.error_details.LocalizedMessage;
+import scala.collection.JavaConverters;
+import scalapb.GeneratedMessage;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class RichErrorNativeImpl implements GreeterService {
+
+    // #rich_error_model_unary
+    @Override
+    public CompletionStage<HelloReply> sayHello(HelloRequest in) {
+
+        ArrayList<scalapb.GeneratedMessage> ar = new ArrayList<>();
+        ar.add(LocalizedMessage.of("EN", "The password!"));
+
+        GrpcServiceException exception = GrpcServiceException.apply(
+                Code.INVALID_ARGUMENT,
+                "What is wrong?",
+                JavaConverters.asScalaBuffer(ar).toSeq()
+        );
+
+        CompletableFuture<HelloReply> future = new CompletableFuture<>();
+        future.completeExceptionally(exception);
+        return future;
+    }
+    // #rich_error_model_unary
+
+    @Override
+    public CompletionStage<HelloReply> itKeepsTalking(Source<HelloRequest, NotUsed> in) {
+        return null;
+    }
+
+    @Override
+    public Source<HelloReply, NotUsed> itKeepsReplying(HelloRequest in) {
+        return null;
+    }
+
+    @Override
+    public Source<HelloReply, NotUsed> streamHellos(Source<HelloRequest, NotUsed> in) {
+        return null;
+    }
+}
