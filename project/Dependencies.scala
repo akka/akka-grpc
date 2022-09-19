94884365312c6de8a921c59cd94d548036c85701
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-09-19 15:02:58 +0200

    Update scala-library to 2.12.17 (#1682)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import buildinfo.BuildInfo
 
 object Dependencies {
   object Versions {
-    val scala212 = "2.12.16"
+    val scala212 = "2.12.17"
     val scala213 = "2.13.8"
 
     // the order in the list is important because the head will be considered the default.
