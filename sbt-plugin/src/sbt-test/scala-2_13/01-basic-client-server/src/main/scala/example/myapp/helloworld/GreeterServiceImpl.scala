ed2b1f12ce642798bfe4fee4ad29e34016db1280
Johan Andrén <johan@markatta.com>
2022-11-14 16:14:34 +0100

    How about them tests

---
+++ sbt-plugin/src/sbt-test/scala-2_13/01-basic-client-server/src/main/scala/example/myapp/helloworld/GreeterServiceImpl.scala
@@ -0,0 +1,24 @@
+package example.myapp.helloworld
+
+import scala.concurrent.Future
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+import example.myapp.helloworld.grpc._
+
+class GreeterServiceImpl extends GreeterService {
+
+  override def sayHello(in: HelloRequest): Future[HelloReply] =
+    Future.successful(HelloReply(s"Hello, ${in.name}!"))
+
+  override def streamHellos(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] =
+    ???
+
+  override def itKeepsTalking(in: Source[HelloRequest, NotUsed]): Future[HelloReply] =
+    ???
+
+  override def itKeepsReplying(in: HelloRequest): Source[HelloReply, NotUsed] =
+    ???
+
+}
