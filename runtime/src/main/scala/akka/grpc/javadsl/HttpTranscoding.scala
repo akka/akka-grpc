88af2fe4fa48bc8a1cf47a2c389b3b146e324dfb
GreyPlane <709327148@qq.com>
2023-09-27 14:47:13 +0800

    using akka grpc interface for javadsl

--- runtime/src/main/scala/akka/grpc/javadsl/HttpTranscoding.scala
+++ runtime/src/main/scala/akka/grpc/javadsl/HttpTranscoding.scala
@@ -2,11 +2,11 @@ package akka.grpc.javadsl
 
 import akka.actor.ClassicActorSystemProvider
 import akka.annotation.ApiMayChange
+import akka.grpc.ServiceDescription
 import akka.grpc.internal.{ HttpTranscoding => InternalTranscoding }
 import akka.http.javadsl.model.{ HttpRequest, HttpResponse }
 import akka.japi.function.{ Function => JFunction }
 import akka.stream.Materializer
-import com.google.protobuf.Descriptors.FileDescriptor
 
 import java.util.concurrent.CompletionStage
 
@@ -14,11 +14,11 @@ object HttpTranscoding {
 
   @ApiMayChange
   def partial(
-      fileDescriptor: FileDescriptor,
+      serviceDescription: ServiceDescription,
       grpcHandler: JFunction[HttpRequest, CompletionStage[HttpResponse]],
       mat: Materializer,
       system: ClassicActorSystemProvider): JFunction[HttpRequest, CompletionStage[HttpResponse]] = {
-
+    val fileDescriptor = serviceDescription.descriptor
     val handlers = InternalTranscoding.serve(fileDescriptor)(mat, system.classicSystem.dispatcher).map {
       case (method, binding) => new HttpHandler(method, binding, grpcHandler)(mat, system.classicSystem.dispatcher)
     }
