3060516f045ee647bb9c77b8ac33a8cceed060fc
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-10-03 12:41:20 +0200

    bump: protobuf 3.21.12 (#1849)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
     // Even referenced explicitly in the sbt-plugin's sbt-tests
     // If changing this, remember to update protoc plugin version to align in
     // maven-plugin/src/main/maven/plugin.xml and akka.grpc.sbt.AkkaGrpcPlugin
-    val googleProtobuf = "3.21.9" // checked synced by VersionSyncCheckPlugin
+    val googleProtobuf = "3.21.12" // checked synced by VersionSyncCheckPlugin
     val googleApi = "2.23.0"
 
     val scalaTest = "3.2.12"
