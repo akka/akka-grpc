e6a64d06ef5ff6f195dbf5152940f7e37f0a685d
GreyPlane <709327148@qq.com>
2023-10-15 14:10:14 +0800

    fix gradle building issues for plugin-tester-java

--- plugin-tester-java/src/test/scala/example/myapp/shelf/ShelfServiceHttpTranscodingSpec.scala
+++ plugin-tester-java/src/test/scala/example/myapp/shelf/ShelfServiceHttpTranscodingSpec.scala
@@ -4,26 +4,39 @@
 
 package example.myapp.shelf
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding.Get
-import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import com.typesafe.config.ConfigFactory
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.Span
 import org.scalatest.wordspec.AnyWordSpecLike
-import spray.json.JsObject
 import spray.json.DefaultJsonProtocol._
+import spray.json.JsObject
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.compat.java8.FutureConverters._
+import scala.concurrent.duration._
 
-class ShelfServiceHttpTranscodingSpec
-    extends ScalaTestWithActorTestKit(config = "akka.http.server.enable-http2 = on")
-    with AnyWordSpecLike
-    with BeforeAndAfterAll {
-  implicit val ec: ExecutionContextExecutor = system.executionContext
+class ShelfServiceHttpTranscodingSpec extends AnyWordSpecLike with Matchers with ScalaFutures with BeforeAndAfterAll {
 
-  val serverWithHttpTranscoding = ShelfServer.run(system.classicSystem).toScala.futureValue
+  implicit val patience: PatienceConfig =
+    PatienceConfig(5.seconds, Span(100, org.scalatest.time.Millis))
+
+  implicit val serverSystem: ActorSystem = {
+    // important to enable HTTP/2 in server ActorSystem's config
+    val conf =
+      ConfigFactory.parseString("akka.http.server.enable-http2 = on").withFallback(ConfigFactory.defaultApplication())
+    val sys = ActorSystem("ShelfServer", conf)
+    // make sure servers are bound before using client
+    ShelfServer.run(sys).toCompletableFuture.get
+    sys
+  }
+
+  implicit val ec: ExecutionContextExecutor = serverSystem.dispatcher
 
   "a gRPC server with HTTP transcoding enabled" should {
 
