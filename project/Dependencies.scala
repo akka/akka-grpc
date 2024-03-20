8e3382f5821ff5f3dd841f00eb8f41e70a8a495a
Scala Steward <scala_steward@virtuslab.com>
2024-03-20 14:19:54 +0000

    bump: scala-library 2.13.13 (was 2.13.12)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ import buildinfo.BuildInfo
 object Dependencies {
   object Versions {
     val scala212 = "2.12.18"
-    val scala213 = "2.13.12"
+    val scala213 = "2.13.13"
     val scala3 = "3.3.1"
 
     // the order in the list is important because the head will be considered the default.
