0eb6e645465ffb171f2fdb088ece16c2a69bb3bc
Johan Andrén <johan@markatta.com>
2024-05-17 13:55:17 +0200

    docs: Use the new akka-http cert loading utils in quickstarts

--- samples/akka-grpc-quickstart-scala/src/main/scala/com/example/helloworld/GreeterServer.scala
+++ samples/akka-grpc-quickstart-scala/src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -2,27 +2,26 @@ package com.example.helloworld
 
 //#import
 
-
 import java.security.KeyStore
 import java.security.SecureRandom
 import java.security.cert.Certificate
 import java.security.cert.CertificateFactory
-
 import scala.io.Source
-
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.ConnectionContext
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.HttpsConnectionContext
+import akka.http.scaladsl.common.SSLContextFactory
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.HttpResponse
 import akka.pki.pem.DERPrivateKeyLoader
 import akka.pki.pem.PEMDecoder
 import com.typesafe.config.ConfigFactory
+
+import java.nio.file.Paths
 import javax.net.ssl.KeyManagerFactory
 import javax.net.ssl.SSLContext
-
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import scala.util.Failure
@@ -30,14 +29,13 @@ import scala.util.Success
 import scala.concurrent.duration._
 //#import
 
-
 //#server
 object GreeterServer {
 
   def main(args: Array[String]): Unit = {
     // important to enable HTTP/2 in ActorSystem's config
-    val conf = ConfigFactory.parseString("akka.http.server.enable-http2 = on")
-      .withFallback(ConfigFactory.defaultApplication())
+    val conf =
+      ConfigFactory.parseString("akka.http.server.enable-http2 = on").withFallback(ConfigFactory.defaultApplication())
     val system = ActorSystem[Nothing](Behaviors.empty[Nothing], "GreeterServer", conf)
     new GreeterServer(system).run()
   }
@@ -51,6 +49,12 @@ class GreeterServer(system: ActorSystem[_]) {
 
     val service: HttpRequest => Future[HttpResponse] =
       GreeterServiceHandler(new GreeterServiceImpl(system))
+
+    val serverHttpContext = ConnectionContext.httpsServer(
+      SSLContextFactory.createSSLContextFromPem(
+        // Note: filesystem paths, not classpath
+        Paths.get("src/main/resources/certs/server1.pem"),
+        Paths.get("src/main/resources/certs/server1.key")))
 
     val bound: Future[Http.ServerBinding] = Http()(system)
       .newServerAt(interface = "127.0.0.1", port = 8080)
@@ -70,34 +74,5 @@ class GreeterServer(system: ActorSystem[_]) {
 
     bound
   }
-  //#server
-
-
-  private def serverHttpContext: HttpsConnectionContext = {
-    val privateKey =
-      DERPrivateKeyLoader.load(PEMDecoder.decode(readPrivateKeyPem()))
-    val fact = CertificateFactory.getInstance("X.509")
-    val cer = fact.generateCertificate(
-      classOf[GreeterServer].getResourceAsStream("/certs/server1.pem")
-    )
-    val ks = KeyStore.getInstance("PKCS12")
-    ks.load(null)
-    ks.setKeyEntry(
-      "private",
-      privateKey,
-      new Array[Char](0),
-      Array[Certificate](cer)
-    )
-    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
-    keyManagerFactory.init(ks, null)
-    val context = SSLContext.getInstance("TLS")
-    context.init(keyManagerFactory.getKeyManagers, null, new SecureRandom)
-    ConnectionContext.httpsServer(context)
-  }
-
-  private def readPrivateKeyPem(): String =
-    Source.fromResource("certs/server1.key").mkString
-  //#server
-
 }
 //#server
