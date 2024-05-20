e9083560f212e039f8d62daa8d5904bc9a9007ee
Johan Andrén <johan@markatta.com>
2024-05-20 09:47:19 +0200

    cleanup imports

--- samples/akka-grpc-quickstart-scala/src/main/scala/com/example/helloworld/GreeterServer.scala
+++ samples/akka-grpc-quickstart-scala/src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -2,31 +2,21 @@ package com.example.helloworld
 
 //#import
 
-import java.security.KeyStore
-import java.security.SecureRandom
-import java.security.cert.Certificate
-import java.security.cert.CertificateFactory
-import scala.io.Source
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.ConnectionContext
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.HttpsConnectionContext
 import akka.http.scaladsl.common.SSLContextFactory
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.HttpResponse
-import akka.pki.pem.DERPrivateKeyLoader
-import akka.pki.pem.PEMDecoder
 import com.typesafe.config.ConfigFactory
 
 import java.nio.file.Paths
-import javax.net.ssl.KeyManagerFactory
-import javax.net.ssl.SSLContext
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.util.Failure
 import scala.util.Success
-import scala.concurrent.duration._
 //#import
 
 //#server
