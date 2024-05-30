ae870418124e18606cfc337b8c1a1ca00d0e4837
Johan Andrén <johan@markatta.com>
2024-05-30 16:48:18 +0200

    docs: Use the new akka-http cert loading utils in quickstarts (#1948)

--- samples/akka-grpc-quickstart-java/src/main/java/com/example/helloworld/GreeterServer.java
+++ samples/akka-grpc-quickstart-java/src/main/java/com/example/helloworld/GreeterServer.java
@@ -5,6 +5,7 @@ package com.example.helloworld;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.http.javadsl.*;
+import akka.http.javadsl.common.SSLContextFactory;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.japi.function.Function;
@@ -16,6 +17,7 @@ import javax.net.ssl.SSLContext;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.PrivateKey;
@@ -52,10 +54,16 @@ public class GreeterServer {
             new GreeterServiceImpl(system),
             system);
 
+    HttpsConnectionContext httpsConnectionContext = ConnectionContext.httpsServer(SSLContextFactory.createSSLContextFromPem(
+            // Note: filesystem paths, not classpath
+            Paths.get("src/main/resources/certs/server1.pem"),
+            Paths.get("src/main/resources/certs/server1.key")
+    ));
+
     CompletionStage<ServerBinding> bound =
             Http.get(system)
                     .newServerAt("127.0.0.1", 8080)
-                    .enableHttps(serverHttpContext())
+                    .enableHttps(httpsConnectionContext)
                     .bind(service);
 
     bound.thenAccept(binding ->
@@ -64,52 +72,5 @@ public class GreeterServer {
 
     return bound;
   }
-  // #server
-
-
-  // FIXME this will be replaced by a more convenient utility, see https://github.com/akka/akka-grpc/issues/89
-  private static HttpsConnectionContext serverHttpContext() throws Exception {
-    String keyEncoded = read(GreeterServer.class.getResourceAsStream("/certs/server1.key"))
-        .replace("-----BEGIN PRIVATE KEY-----\n", "")
-        .replace("-----END PRIVATE KEY-----\n", "")
-        .replace("\n", "");
-
-    byte[] decodedKey = Base64.getDecoder().decode(keyEncoded);
-
-    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decodedKey);
-
-    KeyFactory kf = KeyFactory.getInstance("RSA");
-    PrivateKey privateKey = kf.generatePrivate(spec);
-
-    CertificateFactory fact = CertificateFactory.getInstance("X.509");
-    Certificate cer =
-        fact.generateCertificate(GreeterServer.class.getResourceAsStream("/certs/server1.pem"));
-
-    KeyStore ks = KeyStore.getInstance("PKCS12");
-    ks.load(null);
-    ks.setKeyEntry("private", privateKey, new char[0], new Certificate[]{ cer });
-
-    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-    keyManagerFactory.init(ks, null);
-
-    SSLContext context = SSLContext.getInstance("TLS");
-    context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
-
-    return ConnectionContext.httpsServer(context);
-  }
-
-  private static String read(InputStream in) throws IOException {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream(Math.max(64, in.available()));
-    byte[] buffer = new byte[32 * 1024];
-
-    int bytesRead = in.read(buffer);
-    while (bytesRead >= 0) {
-      baos.write(buffer, 0, bytesRead);
-      bytesRead = in.read(buffer);
-    }
-
-    return new String(baos.toByteArray(), "UTF-8");
-  }
-  //#server
 }
 //#server
