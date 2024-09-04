46cfe333572df16ac45e58ea4f9de25177a0feac
Johan Andrén <johan@markatta.com>
2024-09-04 16:57:52 +0200

    drop that API sketch, it breaks things

--- interop-tests/src/main/scala/akka/grpc/interop/AkkaGrpcClientScala.scala
+++ interop-tests/src/main/scala/akka/grpc/interop/AkkaGrpcClientScala.scala
@@ -4,15 +4,14 @@
 
 package akka.grpc.interop
 
-import scala.concurrent.duration._
 import akka.actor.ActorSystem
-import akka.grpc.GrpcClientSettings
-import akka.http.scaladsl.common.SSLContextFactory
 import io.grpc.internal.testing.TestUtils
-import io.grpc.testing.integration2.{ ClientTester, Settings, TestServiceClient }
+import io.grpc.testing.integration2.ClientTester
+import io.grpc.testing.integration2.Settings
+import io.grpc.testing.integration2.TestServiceClient
 
-import java.nio.file.Paths
 import scala.concurrent.Await
+import scala.concurrent.duration._
 
 // TODO #151 use our own Settings object
 final case class AkkaGrpcClientScala(clientTesterFactory: Settings => ActorSystem => ClientTester) extends GrpcClient {
@@ -24,16 +23,6 @@ final case class AkkaGrpcClientScala(clientTesterFactory: Settings => ActorSyste
 
     val client = new TestServiceClient(clientTesterFactory(settings)(sys))
     client.setUp()
-
-    // alternatively `.withSslContext` directly from a SSLContextFactory.createSSLContextFromPem if not
-    // rotating certs
-    GrpcClientSettings
-      .fromConfig(sys.settings.config)
-      .withSslContextProvider(SSLContextFactory.refreshingSSLContextProvider(5.minutes)(() =>
-        SSLContextFactory.createSSLContextFromPem(
-          certificatePath = Paths.get("/some/path/server.crt"),
-          privateKeyPath = Paths.get("/some/path/server.key"),
-          trustedCaCertificatePaths = Seq(Paths.get("/some/path/serverCA.crt")))))
 
     try client.run(settings)
     finally {
