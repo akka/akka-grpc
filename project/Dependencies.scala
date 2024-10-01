a9aee1f2819697d2989cdb3ed4fee2e8e8d19ffd
Johan Andrén <johan@markatta.com>
2024-10-01 19:32:34 +0200

    bump: Akka 2.9.6 (#1989)
    akka-pki transitively included to be aligned

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     // We don't force Akka updates because downstream projects can upgrade
     // themselves. For more information see
     // https://doc.akka.io//docs/akka/current/project/downstream-upgrade-strategy.html
-    val akka = "2.9.3"
+    val akka = "2.9.6"
     val akkaBinary = "2.9"
     val akkaHttp = "10.6.3"
     val akkaHttpBinary = "10.6"
@@ -114,6 +114,7 @@ object Dependencies {
     Compile.akkaHttpCore,
     Compile.akkaHttp,
     Compile.akkaDiscovery,
+    Compile.akkaPki,
     Test.akkaTestkit,
     Test.akkaStreamTestkit,
     Test.scalaTest,
