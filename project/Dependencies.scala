5c12e7b77f3996aca789e23aca231312eb7fff73
Scala Steward <scala_steward@virtuslab.com>
2025-02-03 17:30:22 +0000

    bump: scala3-library 3.3.5 (was 3.3.4)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   object Versions {
     val scala212 = "2.12.20"
     val scala213 = "2.13.15"
-    val scala3 = "3.3.4"
+    val scala3 = "3.3.5"
 
     // the order in the list is important because the head will be considered the default.
     val CrossScalaForLib = Seq(scala213, scala3)
