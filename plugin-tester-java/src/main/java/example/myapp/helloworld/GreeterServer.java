1959f1fca16d4442a4f7510f9c01f632676f343a
Johan Andrén <johan@markatta.com>
2025-10-28 09:21:19 +0100

    fix: Ensure Java servers are invoked on the specified execution context (#2082)

--- plugin-tester-java/src/main/java/example/myapp/helloworld/GreeterServer.java
+++ plugin-tester-java/src/main/java/example/myapp/helloworld/GreeterServer.java
@@ -22,6 +22,7 @@ import example.myapp.helloworld.grpc.GreeterService;
 import example.myapp.helloworld.grpc.GreeterServiceHandlerFactory;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 class GreeterServer {
@@ -40,6 +41,7 @@ class GreeterServer {
     // ActorSystem threads will keep the app alive until `system.terminate()` is called
   }
 
+  @SuppressWarnings("unchecked")
   public static CompletionStage<ServerBinding> run(ActorSystem sys) throws Exception {
     Materializer mat = SystemMaterializer.get(sys).materializer();
 
@@ -48,7 +50,7 @@ class GreeterServer {
 
     // Create the reflection handler for multiple services
     Function<HttpRequest, CompletionStage<HttpResponse>> reflectionPartial =
-      ServerReflection.create(Arrays.asList(GreeterService.description), sys);
+      ServerReflection.create(List.of(GreeterService.description), sys);
 
     Function<HttpRequest, CompletionStage<HttpResponse>> serviceHandlers =
       ServiceHandler.concatOrNotFound(GreeterServiceHandlerFactory.create(impl, sys), reflectionPartial);
