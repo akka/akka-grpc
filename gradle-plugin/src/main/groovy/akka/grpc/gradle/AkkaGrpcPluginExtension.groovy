3b8f9c4597bec24fbfd61550d64fecd1f858ad22
Johan Andrén <johan@markatta.com>
2025-11-03 09:29:06 +0100

    bump: google protobuf 3.25.8

--- gradle-plugin/src/main/groovy/akka/grpc/gradle/AkkaGrpcPluginExtension.groovy
+++ gradle-plugin/src/main/groovy/akka/grpc/gradle/AkkaGrpcPluginExtension.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 
 class AkkaGrpcPluginExtension {
 
-    static final String PROTOC_VERSION = "3.25.5" // checked synced by VersionSyncCheckPlugin
+    static final String PROTOC_VERSION = "3.25.8" // checked synced by VersionSyncCheckPlugin
 
     static final String PROTOC_PLUGIN_SCALA_VERSION = "2.12"
 
