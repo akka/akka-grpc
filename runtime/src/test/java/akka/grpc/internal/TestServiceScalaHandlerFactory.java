a9c330999e08659ce1f5bc72a6b4ae2fecefa1e1
Johan Andrén <johan@markatta.com>
2025-11-06 14:09:15 +0100

    mima

--- runtime/src/test/java/akka/grpc/internal/TestServiceScalaHandlerFactory.java
+++ runtime/src/test/java/akka/grpc/internal/TestServiceScalaHandlerFactory.java
@@ -22,4 +22,9 @@ public class TestServiceScalaHandlerFactory implements InstancePerRequestFactory
     public PartialFunction<HttpRequest, Future<HttpResponse>> partialInstancePerRequest(Function1<HttpRequest, TestService> serviceFactory, String prefix, PartialFunction<Throwable, Trailers> eHandler, ClassicActorSystemProvider systemProvider, Materializer materializer) {
         return null;
     }
+
+    @Override
+    public PartialFunction<HttpRequest, Future<HttpResponse>> partialInstancePerRequest(Function1<HttpRequest, TestService> serviceFactory, String prefix, PartialFunction<Throwable, Trailers> eHandler, ClassicActorSystemProvider systemProvider) {
+        return null;
+    }
 }
